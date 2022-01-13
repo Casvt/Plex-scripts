9c866ff239ab56a7333f7783315a694709a1afe0
Casvt <88994465+Casvt@users.noreply.github.com>
2022-01-13 20:59:58 +0100

    Made file easier to read

--- stream_control/intro_skipper.py
+++ stream_control/intro_skipper.py
@@ -17,18 +17,6 @@ import re
 import requests
 from plexapi.server import PlexServer
 
-if not re.search('^(\d{1,3}\.){3}\d{1,3}$', plex_ip):
-	print("Error: " + plex_ip + " is not a valid ip")
-	exit(1)
-
-if not re.search('^\d{1,5}$', plex_port):
-	print("Error: " + plex_port + " is not a valid port")
-	exit(1)
-
-if not re.search('^[\w\d_-~]{19,21}$', plex_api_token):
-	print("Error: " + plex_api_token + " is not a valid api token")
-	exit(1)
-
 plex = PlexServer('http://' + plex_ip + ':' + str(plex_port), plex_api_token)
 media_output = {}
 media_chapters = {}
@@ -39,18 +27,22 @@ def move(data):
 	#media is inside chapter marked as advertisement or intro; check if media is played locally as we can only control stream on lan
 	if not data['PlaySessionStateNotification'][0]['ratingKey'] in media_session.keys():
 		for session in json.loads(requests.get('http://' + plex_ip + ':' + plex_port + '/status/sessions', params={'X-Plex-Token': plex_api_token}, headers={'Accept': 'application/json'}).text)['MediaContainer']['Metadata']:
-			if session['sessionKey'] == str(data['PlaySessionStateNotification'][0]['sessionKey']): media_session[data['PlaySessionStateNotification'][0]['ratingKey']] = session
+			if session['sessionKey'] == str(data['PlaySessionStateNotification'][0]['sessionKey']):
+				media_session[data['PlaySessionStateNotification'][0]['ratingKey']] = session
 	if media_session[data['PlaySessionStateNotification'][0]['ratingKey']]['Session']['location'] == 'lan':
 		#media is played on lan; move stream to end of chapter
 		plex.client(media_session[data['PlaySessionStateNotification'][0]['ratingKey']]['Player']['title'], identifier=data['PlaySessionStateNotification'][0]['clientIdentifier']).seekTo(level['endTimeOffset'], mtype='video')
 
 def process(data):
 	if data['type'] == 'playing':
-		print(json.dumps(data, indent=4))
-		if data['PlaySessionStateNotification'][0]['ratingKey'] in media_session.keys() and not media_session[data['PlaySessionStateNotification'][0]['ratingKey']]['Session']['location'] == 'lan': return
-		if not data['PlaySessionStateNotification'][0]['ratingKey'] in media_output.keys(): media_output[data['PlaySessionStateNotification'][0]['ratingKey']] = json.loads(requests.get('http://' + plex_ip + ':' + str(plex_port) + data['PlaySessionStateNotification'][0]['key'], params={'X-Plex-Token': plex_api_token}, headers={'Accept': 'application/json'}).text)
-		if not data['PlaySessionStateNotification'][0]['ratingKey'] in media_chapters.keys() and 'Chapter' in media_output[data['PlaySessionStateNotification'][0]['ratingKey']]['MediaContainer']['Metadata'][0].keys(): media_chapters[data['PlaySessionStateNotification'][0]['ratingKey']] = media_output[data['PlaySessionStateNotification'][0]['ratingKey']]['MediaContainer']['Metadata'][0]['Chapter']
-		if not data['PlaySessionStateNotification'][0]['ratingKey'] in media_markers.keys() and 'Marker' in media_output[data['PlaySessionStateNotification'][0]['ratingKey']]['MediaContainer']['Metadata'][0].keys(): media_markers[data['PlaySessionStateNotification'][0]['ratingKey']] = media_output[data['PlaySessionStateNotification'][0]['ratingKey']]['MediaContainer']['Metadata'][0]['Marker']
+		if data['PlaySessionStateNotification'][0]['ratingKey'] in media_session.keys() and not media_session[data['PlaySessionStateNotification'][0]['ratingKey']]['Session']['location'] == 'lan':
+			return
+		if not data['PlaySessionStateNotification'][0]['ratingKey'] in media_output.keys():
+			media_output[data['PlaySessionStateNotification'][0]['ratingKey']] = json.loads(requests.get('http://' + plex_ip + ':' + str(plex_port) + data['PlaySessionStateNotification'][0]['key'], params={'X-Plex-Token': plex_api_token}, headers={'Accept': 'application/json'}).text)
+		if not data['PlaySessionStateNotification'][0]['ratingKey'] in media_chapters.keys() and 'Chapter' in media_output[data['PlaySessionStateNotification'][0]['ratingKey']]['MediaContainer']['Metadata'][0].keys():
+			media_chapters[data['PlaySessionStateNotification'][0]['ratingKey']] = media_output[data['PlaySessionStateNotification'][0]['ratingKey']]['MediaContainer']['Metadata'][0]['Chapter']
+		if not data['PlaySessionStateNotification'][0]['ratingKey'] in media_markers.keys() and 'Marker' in media_output[data['PlaySessionStateNotification'][0]['ratingKey']]['MediaContainer']['Metadata'][0].keys():
+			media_markers[data['PlaySessionStateNotification'][0]['ratingKey']] = media_output[data['PlaySessionStateNotification'][0]['ratingKey']]['MediaContainer']['Metadata'][0]['Marker']
 		for level in media_chapters[data['PlaySessionStateNotification'][0]['ratingKey']]:
 			if data['PlaySessionStateNotification'][0]['viewOffset'] >= level['startTimeOffset'] and data['PlaySessionStateNotification'][0]['viewOffset'] < level['endTimeOffset'] and 'tag' in level.keys() and level['tag'] == 'Advertisement':
 				move(data)
@@ -63,5 +55,7 @@ def process(data):
 if __name__  == '__main__':
 	try:
 		listener = plex.startAlertListener(callback=process)
-		while True: time.sleep(1)
-	except KeyboardInterrupt: listener.stop()
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		listener.stop()
