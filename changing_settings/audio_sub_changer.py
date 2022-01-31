6cc32f05681b66b02cb8c8e36312ed9cfa4723f5
Casvt <88994465+Casvt@users.noreply.github.com>
2022-01-31 01:23:24 +0100

    Upgraded to V2
    Added multi user support, complete library processing, movie library support, new argument parsing (getopt -> argparse), better language checking, reduced dependencies and removed the need to give the arguments in the correct order.

--- changing_settings/audio_sub_changer.py
+++ changing_settings/audio_sub_changer.py
@@ -1,137 +1,166 @@
 #!/usr/bin/python3
-#The use case of this script is the following:
-#	Change the audio/subtitle track, based on target language, for an episode, season or entire series
-#
-#EXAMPLE USAGE:
-#	python3 audio_sub_changer.py --Type subtitle --Language en --LibraryName Tv-series --Series 'Initial D' --SeasonNumber 5 --EpisodeNumber 1
-#	python3 audio_sub_changer.py --Type audio --Language fr --LibraryName Tv-series --Series 'Into the Night'
+#-*- coding: utf-8 -*-
+
+"""
+The use case of this script is the following:
+	Change the audio/subtitle track, based on target language, for an episode, season, series, movie or entire movie/show library
+Requirements (python3 -m pip install [requirement]):
+	requests
+Setup:
+	Fill the variables below firstly, then run the script.
+	Check the help page (python3 audio_sub_changer.py --help) to see how to use the script with it's arguments.
+	You can find examples of the usage at the bottom of the help page.
+	When the script is run with it's arguments, it will show you what media it has processed and will let you know for what media it has changed and for which users
+"""
 
 plex_ip = ''
 plex_port = ''
 plex_api_token = ''
 
-import re
+import requests, argparse
 
-if not re.search('^(\d{1,3}\.){3}\d{1,3}$', plex_ip):
-	print("Error: " + plex_ip + " is not a valid ip")
-	exit(1)
+def episode(episode_id, media_type="show"):
+	for token in users:
+		episode_output = requests.get(baseurl + '/library/metadata/' + str(episode_id), params={'X-Plex-Token': token}, headers={'Accept': 'application/json'}).json()
+		part_db = {}
+		if media_type == "show" and token == users[0]:
+			print(episode_output['MediaContainer']['Metadata'][0]['grandparentTitle'] + ' - S' + str(episode_output['MediaContainer']['Metadata'][0]['parentIndex']) + 'E' + str(episode_output['MediaContainer']['Metadata'][0]['index']) + ' - ' + str(episode_output['MediaContainer']['Metadata'][0]['title']))
+		elif media_type == "movie" and token == user[0]:
+			print(episode_output['MediaContainer']['Metadata'][0]['title'])
+		for media in episode_output['MediaContainer']['Metadata'][0]['Media']:
+			for part in media['Part']:
+				selected_stream = ''
+				for stream in part['Stream']:
+					if stream['streamType'] == args.Type and 'selected' in stream.keys() and 'languageTag' in stream.keys() and stream['languageTag'] == args.Language:
+						selected_stream = stream
+						break
+				part_db[part['id']] = {
+					'part': part,
+					'selected_stream': selected_stream
+				}
+		for part in part_db.keys():
+			for stream in part_db[part]['part']['Stream']:
+				if stream['streamType'] == args.Type and 'languageTag' in stream.keys() and stream['languageTag'] == args.Language and not part_db[part]['selected_stream']:
+					if args.Type == 2:
+						requests.put(baseurl + '/library/parts/' + str(part), params={'audioStreamID': stream['id'], 'allParts': 1, 'X-Plex-Token': token})
+					elif args.Type == 3:
+						requests.put(baseurl + '/library/parts/' + str(part), params={'subtitleStreamID': stream['id'], 'allParts': 1, 'X-Plex-Token': token})
+					print(f'	Edited for {users_name[token]}')
+					break
 
-if not re.search('^\d{1,5}$', plex_port):
-	print("Error: " + plex_port + " is not a valid port")
-	exit(1)
-
-if not re.search('^[\w\d_-~]{19,21}$', plex_api_token):
-	print("Error: " + plex_api_token + " is not a valid api token")
-	exit(1)
-
-import requests
-import json
-import getopt
-import sys
-
-def episode(episode_id):
-	episode_output = json.loads(ssn.get(baseurl + '/library/metadata/' + str(episode_id)).text)
-	part_id = []
-	part_db = {}
-	print(episode_output['MediaContainer']['Metadata'][0]['grandparentTitle'] + ' - S' + str(episode_output['MediaContainer']['Metadata'][0]['parentIndex']) + 'E' + str(episode_output['MediaContainer']['Metadata'][0]['index']) + ' - ' + str(episode_output['MediaContainer']['Metadata'][0]['title']))
-	for media in episode_output['MediaContainer']['Metadata'][0]['Media']:
-		for part in media['Part']:
-			part_db[part['id']] = part
-			part_id.append(part['id'])
-	for part in part_id:
-		selected_stream = ''
-		for stream in part_db[part]['Stream']:
-			if stream['streamType'] == int(type_number) and 'selected' in stream.keys() and 'languageTag' in stream.keys() and stream['languageTag'] == lang:
-				selected_stream = stream
-				break
-		for stream in part_db[part]['Stream']:
-			if stream['streamType'] == int(type_number) and 'languageTag' in stream.keys() and stream['languageTag'] == lang and not selected_stream:
-				print('	Edited')
-				if type == 'audio':
-					ssn.put(baseurl + '/library/parts/' + str(part), params={'audioStreamID': stream['id'], 'allParts': 1})
-				elif type == 'subtitle':
-					ssn.put(baseurl + '/library/parts/' + str(part), params={'subtitleStreamID': stream['id'], 'allParts': 1})
-				break
-
-
+#setup variables
 ssn = requests.Session()
 ssn.headers.update({'Accept': 'application/json'})
 ssn.params.update({'X-Plex-Token': plex_api_token})
 baseurl = 'http://' + plex_ip + ':' + plex_port
+section_output = ssn.get(baseurl + '/library/sections').json()
+langs = ['aa', 'ab', 'af', 'ak', 'am', 'an', 'ar', 'as', 'av', 'ay', 'az', 'ba', 'be', 'bg', 'bh', 'bi', 'bm', 'bn', 'bo', 'br', 'bs', 'ca', 'ce', 'ch', 'co', 'cr', 'cs', 'cu', 'cv', 'cy', 'da', 'de', 'dv', 'dz', 'ee', 'el', 'en', 'en-US', 'en-GB', 'en-AU', 'eo', 'es', 'et', 'eu', 'fa', 'ff', 'fi', 'fj', 'fo', 'fr', 'fy', 'ga', 'gd', 'gl', 'gn', 'gu', 'gv', 'ha', 'he', 'hi', 'ho', 'hr', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'io', 'is', 'it', 'iu', 'ja', 'jv', 'ka', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'kr', 'ks', 'ku', 'kv', 'kw', 'ky', 'la', 'lb', 'lg', 'li', 'ln', 'lo', 'lt', 'lu', 'lv', 'mg', 'mh', 'mi', 'mk', 'ml', 'mn', 'mo', 'mr', 'ms', 'mt', 'my', 'na', 'nb', 'nd', 'ne', 'ng', 'nl', 'nn', 'no', 'nr', 'nv', 'ny', 'oc', 'oj', 'om', 'or', 'os', 'pa', 'pi', 'pl', 'ps', 'pt', 'qu', 'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sc', 'sd', 'se', 'sg', 'sh', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st', 'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'wo', 'xh', 'yi', 'yo', 'za', 'zh', 'zu']
 
-section_output = json.loads(ssn.get(baseurl + '/library/sections').text)
-arguments, values = getopt.getopt(sys.argv[1:], 'ht:l:L:S:s:e:', ['Help', 'Type=', 'Language=', 'LibraryName=', 'Series=', 'SeasonNumber=', 'EpisodeNumber='])
-type = ''
-lang = ''
-lib_id = ''
-series_id = ''
-season_id = ''
-episode_id = ''
-for argument, value in arguments:
-	if argument in ('-h', '--Help'):
-		print('The arguments to use this script:\nIMPORTANT: The arguments need to be given in the order that they are shown here!')
-		print('Required: -t/--Type [audio|subtitle], -l/--Language [ISO-639-1 (2-lowercase-letters) language code (e.g. en)], -L/--LibraryName [name of target library], -S/--Series [target series name]')
-		print('Optional: -s/--SeasonNumber [number of the target season], -e/--EpisodeNumber [number of the target episode]')
-		print('When --Series is given, the whole series is processed; when --SeasonNumber is given, the whole season of the series is processed; when --EpisodeNumber is given, that specific episode of that specific season is processed')
-		exit()
-	if argument in ('-t', '--Type'):
-		if re.search('^(audio|subtitle)$', value):
-			type = value
-			if type == 'audio':
-				type_number = 2
-			elif type == 'subtitle':
-				type_number = 3
-		else:
-			print('Error: type given is not audio or subtitle')
-			exit(1)
+#handle argument parsing
+parser = argparse.ArgumentParser(
+	formatter_class=argparse.RawDescriptionHelpFormatter,
+	description="Change the audio/subtitle track, based on target language, for an episode, season, series, movie or entire movie/show library\n\n\
+You can narrow down the media that is processed (normally the complete library) by specifying the series, season or even specific episode that you want to process for a show library,\n\
+or a movie for a movie library. If you've selected a movie library, you can parse the -m/--Movie argument multiple times to process multiple movies.",
+	epilog=f"Examples:\n\
+	python3 {__file__} --Type audio --Language fr --LibraryName Tv-series\n\
+		Try to set the audio for all episodes of all series in the 'Tv-series' library to one with the language French\n\
+	python3 {__file__} --Type subtitle --Language en --LibraryName Tv-series --Series 'Initial D' --SeasonNumber 5\n\
+		Try to set the subtitle for Tv-series->Initial D->S05 to one with the language English\n\
+	python3 {__file__} --Type subtitle --Language en --LibraryName Films --Movie '2 Fast 2 Furious' --Movie 'The Fast and The Furious'\n\
+		Try to set the subtitle for Films->2 Fast 2 Furious AND ALSO Films->The Fast and The Furious to one with the language English"
+)
+parser.add_argument('-t', '--Type', choices=['audio','subtitle'], help="Give the type of stream to change", required=True)
+parser.add_argument('-l', '--Language', type=str, help="ISO-639-1 (2 lowercase letters) language code (e.g. en) to try to set the stream to", required=True)
+parser.add_argument('-L', '--LibraryName', type=str, help="Name of target library", required=True)
+parser.add_argument('-S', '--Series', type=str, help="Target series name")
+parser.add_argument('-s', '--SeasonNumber', type=int, help="Target Season number")
+parser.add_argument('-e', '--EpisodeNumber', type=int, help="Target Episode number")
+parser.add_argument('-m', '--Movie', type=str, help="Target movie; allowed to give argument multiple times", action='append')
+parser.add_argument('-u', '--User', type=str, help="Select the user(s) to apply this to; Give username, '@me' for yourself or '@all' for everyone", action='append')
+args = parser.parse_args()
+if args.Type == 'audio': args.Type = 2
+elif args.Type == 'subtitle': args.Type = 3
+if not args.Language in langs: parser.error('-l/--Language requires a valid language code')
+if isinstance(args.SeasonNumber, int) and (args.Series == None): parser.error('-s/--SeasonNumber requires -S/--Series')
+if isinstance(args.EpisodeNumber, int) and (args.Series == None or args.SeasonNumber == None): parser.error('-e/--EpisodeNumber requires -S/--Series and -s/--SeasonNumber')
+for level in section_output['MediaContainer']['Directory']:
+	if level['title'] == args.LibraryName and level['type'] in ('show','movie'):
+		args.LibraryName = level['key']
+		break
+else: parser.error('Library not found')
+if args.Series != None:
+	for level in ssn.get(baseurl + '/library/sections/' + args.LibraryName + '/all').json()['MediaContainer']['Metadata']:
+		if level['title'] == args.Series:
+			args.Series = level['ratingKey']
+			break
+	else: parser.error('Series not found')
+if args.SeasonNumber != None:
+	for level in ssn.get(baseurl + '/library/metadata/' + args.Series + '/children').json()['MediaContainer']['Metadata']:
+		if level['index'] == args.SeasonNumber:
+			args.SeasonNumber = level['ratingKey']
+			break
+	else: parser.error('Season not found')
+if args.EpisodeNumber != None:
+	for level in ssn.get(baseurl + '/library/metadata/' + args.SeasonNumber + '/children').json()['MediaContainer']['Metadata']:
+		if level['index'] == args.EpisodeNumber:
+			args.EpisodeNumber = level['ratingKey']
+			break
+	else: parser.error('Episode not found')
+users = []
+users_name = {}
+if args.User == None:
+	users = [plex_api_token]
+	users_name = {plex_api_token: 'yourself'}
+else:
+	import re
+	user_share_output = requests.get('http://plex.tv/api/servers/' + ssn.get(baseurl + '/').json()['MediaContainer']['machineIdentifier'] + '/shared_servers', params={'X-Plex-Token': plex_api_token}).text
+	if '@all' in args.User:
+		for user_data in re.findall('(?<=username=").*accessToken=".*?(?=" )', user_share_output):
+			user_token = str(re.search('[^"]+$', user_data).group(0))
+			users.append(user_token)
+			users_name[user_token] = str(re.search('^.*?(?=" )', user_data).group(0))
+	else:
+		for user in args.User:
+			if user == '@me':
+				users.append(plex_api_token)
+				users_name[plex_api_token] = 'yourself'
+				continue
+			else:
+				try:
+					pre_user_token = str(re.search('(?<=username="' + user + '").*accessToken=".*?(?=")', user_share_output).group(0))
+					user_token = str(re.search('[^"]+$', pre_user_token).group(0))
+					users.append(user_token)
+					users_name[user_token] = str(re.search('^.*?(?=" )', user_data).group(0))
 
-	if argument in ('-l', '--Language'):
-		if re.search('^\S{2,5}$', value): lang = value
-		else:
-			print('Error: language given is not a valid language code')
-			exit(1)
+				except AttributeError:
+					parser.error(f'{user} not found on server')
 
-	if argument in ('-L', '--LibraryName'):
-		for level in section_output['MediaContainer']['Directory']:
-			if level['title'] == str(value) and level['type'] == 'show': lib_id = level['key']
-		if not lib_id:
-			print('Error: library not found or not a show library')
-			exit(1)
-
-	if argument in ('-S', '--Series'):
-		for level in json.loads(ssn.get(baseurl + '/library/sections/' + lib_id + '/all').text)['MediaContainer']['Metadata']:
-			if level['title'] == str(value): series_id = level['ratingKey']
-		if not series_id:
-			print('Error: series not found or arguments not given in correct order')
-			exit(1)
-
-	if argument in ('-s', '--SeasonNumber'):
-		for level in json.loads(ssn.get(baseurl + '/library/metadata/' + series_id + '/children').text)['MediaContainer']['Metadata']:
-			if level['index'] == int(value): season_id = level['ratingKey']
-		if not season_id:
-			print('Error: season not found or arguments not given in correct order')
-			exit(1)
-
-	if argument in ('-e', '--EpisodeNumber'):
-		for level in json.loads(ssn.get(baseurl + '/library/metadata/' + season_id + '/children').text)['MediaContainer']['Metadata']:
-			if level['index'] == int(value): episode_id = level['ratingKey']
-		if not episode_id:
-			print('Error: episode not found or arguments not given in correct order')
-			exit(1)
-
-if not lib_id or not lang or not type or not series_id:
-	print('Error: Required arguments were not all given\nrun audio_sub_changer.py -h')
-	exit(1)
-
-if episode_id:
+#edit the media
+if args.EpisodeNumber != None:
 	#change an episode
-	episode(episode_id)
-elif season_id:
+	episode(args.EpisodeNumber)
+elif args.SeasonNumber != None:
 	#change a season
-	for episodes in json.loads(ssn.get(baseurl + '/library/metadata/' + season_id + '/children').text)['MediaContainer']['Metadata']:
+	for episodes in ssn.get(baseurl + '/library/metadata/' + args.SeasonNumber + '/children').json()['MediaContainer']['Metadata']:
 		episode(episodes['ratingKey'])
-elif series_id:
+elif args.Series != None:
 	#change a series
-	for seasons in json.loads(ssn.get(baseurl + '/library/metadata/' + series_id + '/children').text)['MediaContainer']['Metadata']:
-		for episodes in json.loads(ssn.get(baseurl + '/library/metadata/' + seasons['ratingKey'] + '/children').text)['MediaContainer']['Metadata']:
-			episode(episodes['ratingKey'])
+	for episodes in ssn.get(baseurl + '/library/metadata/' + args.Series + '/allLeaves').json()['MediaContainer']['Metadata']:
+		episode(episodes['ratingKey'])
+else:
+	lib_output = ssn.get(baseurl + '/library/sections/' + args.LibraryName + '/all').json()
+	if lib_output['MediaContainer']['Metadata'][0]['type'] == 'show':
+		#change a complete show library
+		for show in lib_output['MediaContainer']['Metadata']:
+			for episodes in ssn.get(baseurl + '/library/metadata/' + show['ratingKey'] + '/allLeaves').json()['MediaContainer']['Metadata']:
+				episode(episodes['ratingKey'])
+	elif lib_output['MediaContainer']['Metadata'][0]['type'] == 'movie':
+		#change a complete movie library
+		if args.Series != None or args.SeasonNumber != None or args.EpisodeNumber != None:
+			parser.error('Library is a movie library but show-arguments were given')
+		for movie in lib_output['MediaContainer']['Metadata']:
+			if args.Movie != None and not str(movie['title']) in args.Movie: continue
+			episode(movie['ratingKey'], media_type="movie")
+	else:
+		parser.error('Library is not a show or movie library')
