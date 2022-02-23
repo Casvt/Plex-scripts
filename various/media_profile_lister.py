4d360c8b81e83c31cc143c8309d5bcb663684bfc
Casvt <88994465+Casvt@users.noreply.github.com>
2022-02-23 14:22:25 +0100

    Updated script to V2
    Improved documentation, reduced dependencies, improved argument parsing, reduced web requests made for series significantly

--- various/media_profile_lister.py
+++ various/media_profile_lister.py
@@ -1,61 +1,52 @@
-#The use case of this script is the following:
-#	After selecting a library and giving a quality profile (e.g. main 10),
-#	list all movies/episodes that match that profile.
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+"""
+The use case of this script is the following:
+	After selecting a library and giving a quality profile (e.g. main 10),
+	the script will list all movies/episodes that match that profile.
+Requirements (python3 -m pip install [requirement]):
+	requests
+Setup:
+	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+"""
 
 plex_ip = ''
 plex_port = ''
 plex_api_token = ''
 
-import requests
-import json
-import re
-import getopt, sys
+import requests, argparse
 
-if not re.search('^(\d{1,3}\.){3}\d{1,3}$', plex_ip):
-	print("Error: " + plex_ip + " is not a valid ip")
-	exit(1)
-
-if not re.search('^\d{1,5}$', plex_port):
-	print("Error: " + plex_port + " is not a valid port")
-	exit(1)
-
-if not re.search('^[\w\d_-~]{19,21}$', plex_api_token):
-	print("Error: " + plex_api_token + " is not a valid api token")
-	exit(1)
-
+base_url = f'http://{plex_ip}:{plex_port}'
 ssn = requests.Session()
 ssn.headers.update({'Accept': 'application/json'})
 ssn.params.update({'X-Plex-Token': plex_api_token})
+parser = argparse.ArgumentParser(description="List all movies or episodes that have a certain quality profile")
+parser.add_argument('-l','--LibraryName', help="Name of target library", required=True)
+parser.add_argument('-p','--Profile', help="Profile of media to show (e.g. 'main 10')", required=True)
+args = parser.parse_args()
 
-section_output = json.loads(ssn.get('http://' + plex_ip + ':' + plex_port + '/library/sections').text)
-arguments, values = getopt.getopt(sys.argv[1:], 'l:p:', ['LibraryName=', 'Profile='])
 lib_id = ''
 lib_type = ''
-profile = ''
-for argument, value in arguments:
-	if argument in ('-l', '--LibraryName'):
-		for level in section_output['MediaContainer']['Directory']:
-			if level['title'] == value:
-				if level['type'] == 'movie' or level['type'] == 'show':
-					lib_id = level['key']
-					lib_type = level['type']
-		if not lib_id:
-			print('Library not found or not a movie/show library')
-			exit(1)
-	if argument in ('-p', '--Profile'): profile = value
-
-if not lib_id or not profile:
-	print('Error: Arguments were not all given')
-	print('Required: -l/--LibraryName [name of target library], -p/--Profile [profile of media to show (e.g. \'main 10\')]')
-	exit(1)
+section_output = ssn.get(f'{base_url}/library/sections').json()
+for lib in section_output['MediaContainer']['Directory']:
+	if lib['title'] == args.LibraryName:
+		if lib['type'] in ('movie','show'):
+			lib_id = lib['key']
+			lib_type = lib['type']
+			break
+		else:
+			parser.error('Library is not a show or movie library')
+else:
+	parser.error('Library not found')
 
 if lib_type == 'movie':
-	for movie in json.loads(ssn.get('http://' + plex_ip + ':' + plex_port + '/library/sections/' + lib_id + '/all').text)['MediaContainer']['Metadata']:
-		if 'videoProfile' in movie['Media'][0] and movie['Media'][0]['videoProfile'] == profile: print(movie['title'])
+	for movie in ssn.get(f'{base_url}/library/sections/{lib_id}/all').json()['MediaContainer']['Metadata']:
+		if 'videoProfile' in movie['Media'][0].keys() and movie['Media'][0]['videoProfile'] == args.Profile:
+			print(movie['title'])
+
 elif lib_type == 'show':
-	for show in json.loads(ssn.get('http://' + plex_ip + ':' + plex_port + '/library/sections/' + lib_id + '/all').text)['MediaContainer']['Metadata']:
-		show_key = show['key']
-		for season in json.loads(ssn.get('http://' + plex_ip + ':' + plex_port + show_key).text)['MediaContainer']['Metadata']:
-			season_key = season['key']
-			for episode in json.loads(ssn.get('http://' + plex_ip + ':' + plex_port + season_key).text)['MediaContainer']['Metadata']:
-				if 'videoProfile' in episode['Media'][0] and episode['Media'][0]['videoProfile'] == profile: print(episode['grandparentTitle'] + ' - S' + str(episode['parentIndex']) + 'E' + str(episode['index']) + ' - ' + str(episode['title']))
+	for show in ssn.get(f'{base_url}/library/sections/{lib_id}/all').json()['MediaContainer']['Metadata']:
+		for episode in ssn.get(f'{base_url}/library/metadata/{show["ratingKey"]}/allLeaves').json()['MediaContainer']['Metadata']:
+			if 'videoProfile' in episode['Media'][0].keys() and episode['Media'][0]['videoProfile'] == args.Profile:
+				print(f'{episode["grandparentTitle"]} - S{episode["parentIndex"]}E{episode["index"]} - {episode["title"]}')
