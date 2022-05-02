a7ff43f0f7f2320838cd35f7fbf3ef1f13bb0b38
Cas van Tijn <casvantijn@gmail.com>
2022-05-02 15:24:07 +0000

    Improved importibility, effeciency and uniformity

--- changing_settings/first_photo_album_cover.py
+++ changing_settings/first_photo_album_cover.py
@@ -3,88 +3,114 @@
 
 """
 The use case of this script is the following:
-	The first image in an album will be made the cover of the album.
-	This script applies to every album in every directory (a.k.a every album that exists on your plex server)
+	The first image in an album will be made the cover of the album
 Requirements (python3 -m pip install [requirement]):
 	requests
 Setup:
-	Fill the variables below firstly, then run the script.
-	Run this script every once in a while. Every time it is run, the album covers will be updated.
-	You can run it every 15 minutes if the cover is important or every 12 hours if you want to keep load low
+	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+	Run this script at an interval. Decide for yourself what the interval is (e.g. every 20m or every 12h)
 """
-
-import requests
-import re
 
 plex_ip = ''
 plex_port = ''
 plex_api_token = ''
-#add names of albums to exclude (e.g. if you add 'Album1', albums with the name Album1 will not be processed -> exclusion_name = ['Album1'])
-exclusion_name = []
-#add names of albums to ONLY process (e.g. if you only want to process albums with the name 'Album2', add it to the list below -> inclusion_name = ['Album2'])
-#IF INCLUSION_NAME HAS VALUES, EXCLUSION_NAME WILL BE IGNORED
-inclusion_name = []
-#add regexes to match against; albums that match regex will be ignored (e.g. exclusion_regex = ['Album\d{1,3}'])
-exclusion_regex = []
-#add regexes to match against; only albums that match the regex will be processed (e.g. inclusion_regex = ['Album\d{1,3}'])
-#IF INCLUSION_REGEX HAS VALUES, EXCLUSION_REGEX WILL BE IGNORED
-inclusion_regex = []
-#add ratingkeys of albums to exclude from processing (e.g. exclusion_key = ['1666' '123'])
-exclusion_key = []
-#add ratingkeys of albums to ONLY process (e.g. inclusion_key = ['133' '46'])
-#IF INCLUSION_KEY HAS VALUES, EXCLUSION_KEY WILL BE IGNORED
-inclusion_key = []
 
-def reg_loop_ex():
-	for regex in exclusion_regex:
-		if re.search(regex, album_title): return 1
+from os import getenv
+from re import match as re_match
 
-def reg_loop_in():
-	for regex in inclusion_regex:
-		if not re.search(regex, album_title): return 1
+# Environmental Variables
+plex_ip = getenv('plex_ip', plex_ip)
+plex_port = getenv('plex_port', plex_port)
+plex_api_token = getenv('plex_api_token', plex_api_token)
+base_url = f"http://{plex_ip}:{plex_port}"
 
-def recursive_albums(album):
+#keeps track of covers of albums including new covers
+album_images = {}
+
+def _process_album(ssn, album_key: str):
 	album_image = ''
-	album_output = ssn.get('http://' + plex_ip + ':' + plex_port + album).json()
-	album_title = album_output['MediaContainer']['parentTitle']
-	#check if the album falls under any of the rules
-	if reg_loop_ex() == 1: return
-	if album_title in exclusion_name: return
-	if reg_loop_in() == 1: return
-	if inclusion_name and not album_title in inclusion_name: return
-	if album_output['MediaContainer']['key'] in exclusion_key: return
-	if inclusion_key and not album_output['MediaContainer']['key'] in inclusion_key: return
-	#if there are sub-albuns, do them first
-	for level in album_output['MediaContainer']['Metadata']:
-		if not 'Media' in level.keys():
-			recursive_albums(level['key'])
-		elif not album_image: album_image = level
-	#if there is a picture present in the album, upload it
+
+	album_output = ssn.get(f'{base_url}{album_key}').json()['MediaContainer']['Metadata']
+	#if there are sub-albums, do them first but also note first picture in album
+	for image in album_output:
+		if not 'Media' in image.keys():
+			_process_album(image['key'])
+		elif not album_image:
+			album_image = image
+
 	if album_image:
+		#there is a picture present in the album, make it the cover
 		album_images[album_image['parentRatingKey']] = album_image['Media'][0]['Part'][0]['file']
-		requests.post(f'http://{plex_ip}:{plex_port}/library/metadata/{album_image["parentRatingKey"]}/posters', data=open(album_image['Media'][0]['Part'][0]['file'], 'rb').read())
+		ssn.post(f'{base_url}/library/metadata/{album_image["parentRatingKey"]}/posters', data=open(album_image['Media'][0]['Part'][0]['file'], 'rb').read(), headers={})
 	else:
 		#there isnt a picture in the album so use the cover of the first sub-album
-		for level in album_output['MediaContainer']['Metadata']:
-			if not 'Media' in level.keys():
-				album_images[level['parentRatingKey']] = album_images[level['ratingKey']]
-				requests.post(f'http://{plex_ip}:{plex_port}/library/metadata/{level["parentRatingKey"]}/posters', data=open(album_images[level['ratingKey']], 'rb').read())
+		for image in album_output:
+			if not 'Media' in image.keys():
+				album_images[image['parentRatingKey']] = album_images[image['ratingKey']]
+				ssn.post(f'{base_url}/library/metadata/{image["parentRatingKey"]}/posters', data=open(album_images[image['ratingKey']], 'rb').read(), headers={})
 				break
 
-album_images = {}
-if inclusion_name: exclusion_name = []
-if inclusion_regex: exclusion_regex = []
-if inclusion_key: exclusion_key = []
+def first_photo_album_cover(ssn, library_name: str, exclude_name: list=[], exclude_regex: list=[], include_name: list=[], include_regex: list=[]):
+	result_json = []
 
-ssn = requests.Session()
-ssn.headers.update({'Accept': 'application/json'})
-ssn.params.update({'X-Plex-Token': plex_api_token})
-base_url = 'http://' + plex_ip + ':' + plex_port
+	#check for illegal argument parsing
+	if include_name: exclude_name = []
+	if include_regex: exclude_regex = []
 
-resp = ssn.get(base_url + '/library/sections').json()['MediaContainer']['Directory']
-lib_keys = [lib['key'] for lib in resp if lib['type'] == 'photo']
+	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer']['Directory']
+	#loop through the libraries
+	for lib in sections:
+		if lib['title'] != library_name: continue
 
-for key in lib_keys:
-	album_keys = [folders['key'] for folders in ssn.get(base_url + '/library/sections/' + key + '/all').json()['MediaContainer']['Metadata']]
-	for album in album_keys:
-		recursive_albums(album)
+		#this library is targeted
+		print(lib['title'])
+		if lib['type'] == 'photo':
+			lib_output = ssn.get(f'{base_url}/library/sections/{lib["key"]}/all').json()['MediaContainer']['Metadata']
+			#loop through every album in the library
+			for album in lib_output:
+				#check if album is allowed to be processed
+				if include_name and album['title'] not in include_name: continue
+				if exclude_name and album['title'] in exclude_name: continue
+				if include_regex:
+					for regex in include_regex:
+						if re_match(regex, album['title']):
+							break
+					else: continue
+				if exclude_regex:
+					skip_album = False
+					for regex in exclude_regex:
+						if re_match(regex, album['title']):
+							skip_album = True
+							break
+					if skip_album == True: continue
+
+				#process album
+				print(f'	{album["title"]}')
+				_process_album(ssn, album['key'])
+				result_json.append(album['key'])
+		else:
+			return 'Library not supported'
+
+	return result_json
+
+if __name__ == '__main__':
+	import requests, argparse
+
+	#setup vars
+	ssn = requests.Session()
+	ssn.headers.update({'Accept': 'application/json'})
+	ssn.params.update({'X-Plex-Token': plex_api_token})
+
+	#setup arg parsing
+	parser = argparse.ArgumentParser(description='Set the first image of an album as the album cover')
+	parser.add_argument('-l','--LibraryName', type=str, help='Name of target library', required=True)
+	parser.add_argument('-e','--ExcludeName', type=str, help='Give name of album to exclude from processing; allowed to give argument multiple times', action='append', default=[])
+	parser.add_argument('-E','--ExcludeRegex', type=str, help='Give regex where matching album names are excluded from processing; allowed to give argument multiple times', action='append')
+	parser.add_argument('-i','--IncludeName', type=str, help='Give name of album to only process; overrides -e/--ExcludeName; allowed to give argument multiple times', action='append', default=[])
+	parser.add_argument('-I','--IncludeRegex', type=str, help='Give regex where only matching albums are processed; overrides -E/--ExcludeRegex; allowed to give argument multiple times', action='append', default=[])
+
+	args = parser.parse_args()
+	#call function and process result
+	response = first_photo_album_cover(ssn=ssn, library_name=args.LibraryName, exclude_name=args.ExcludeName, exclude_regex=args.ExcludeRegex, include_name=args.IncludeName, include_regex=args.IncludeRegex)
+	if not isinstance(response, list):
+		parser.error(response)
