64ac0e9b5375be9e3b99dc6f090215b4fe08c445
Cas van Tijn <casvantijn@gmail.com>
2022-05-11 15:24:36 +0000

    Improved importibility and uniformity

--- playlist_collection/m3u_to_playlist.py
+++ playlist_collection/m3u_to_playlist.py
@@ -3,7 +3,7 @@
 
 """
 The use case of this script is the following:
-	Converting an m3u file to a plex playlist
+	Convert a .m3u file to a plex playlist
 Requirements (python3 -m pip install [requirement]):
 	requests
 Setup:
@@ -14,27 +14,45 @@ plex_ip = ''
 plex_port = ''
 plex_api_token = ''
 
-import requests, argparse
+from os import getenv
 from os import path
 
-base_url = f'http://{plex_ip}:{plex_port}'
-ssn = requests.Session()
-ssn.headers.update({'Accept':'application/json'})
-ssn.params.update({'X-Plex-Token':plex_api_token})
+# Environmental Variables
+plex_ip = getenv('plex_ip', plex_ip)
+plex_port = getenv('plex_port', plex_port)
+plex_api_token = getenv('plex_api_token', plex_api_token)
+base_url = f"http://{plex_ip}:{plex_port}"
 
-section_output = ssn.get(f'{base_url}/library/sections').json()
-parser = argparse.ArgumentParser(description="Script to convert a .m3u file to a plex playlist")
-parser.add_argument('-l','--LibraryName', help="Name of target library", required=True)
-parser.add_argument('-f','--File', help="Path to .m3u file", required=True)
-args = parser.parse_args()
-lib_id = ''
-for lib in section_output['MediaContainer']['Directory']:
-	if lib['title'] == args.LibraryName:
-		lib_id = lib['key']
-		break
-else:
-	parser.error('Library not found')
-if not path.exists(args.File):
-	parser.error('File not found')
+def m3u_to_playlist(ssn, library_name: str, file_path: str):
+	#check for illegal arg parsing
+	if not path.isfile(file_path):
+		return 'File not found'
 
-ssn.post(f'{base_url}/playlists/upload', params={'sectionID': lib_id, 'path': args.File})
+	#loop through the libraries
+	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer']['Directory']
+	for lib in sections:
+		if lib['title'] == library_name:
+			ssn.post(f'{base_url}/playlists/upload', params={'sectionID': lib['key'], 'path': file_path})
+			break
+	else:
+		return 'Library not found'
+	return 
+
+if __name__ == '__main__':
+	import requests, argparse
+
+	#setup vars
+	ssn = requests.Session()
+	ssn.headers.update({'Accept': 'application/json'})
+	ssn.params.update({'X-Plex-Token': plex_api_token})
+
+	#setup arg parsing
+	parser = argparse.ArgumentParser(description='Set the first image of an album as the album cover')
+	parser.add_argument('-l','--LibraryName', type=str, help='Name of target library', required=True)
+	parser.add_argument('-f','--File', type=str, help="File path to the .m3u file", required=True)
+
+	args = parser.parse_args()
+	#call function and process result
+	response = m3u_to_playlist(ssn=ssn, library_name=args.LibraryName, file_path=args.File)
+	if not response == None:
+		parser.error(response)
