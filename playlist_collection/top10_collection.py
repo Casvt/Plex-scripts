855f08afe9e719f0497d9e14b4c2ed4fe86cd4ce
Cas van Tijn <casvantijn@gmail.com>
2022-05-11 15:42:55 +0000

    Improved importability and uniformity

--- playlist_collection/top10_collection.py
+++ playlist_collection/top10_collection.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+#-*- coding: utf-8 -*-
 
-#The use case of this script is the following:
-#	Run this script to add a collection with the 10 most popular movies according to tautulli
-#	It is intented to run every so often (use crontab to run it every 12 hours for example)
+"""
+The use case of this script is the following:
+	Make a collection with the top 10 most popular movies according to tautulli
+Requirements (python3 -m pip install [requirement]):
+	requests
+Setup:
+	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+	Run this script at an interval. Decide for yourself what the interval is (e.g. every 20m or every 12h)
+"""
 
 plex_ip = ''
 plex_port = ''
@@ -12,65 +19,65 @@ tautulli_ip = ''
 tautulli_port = ''
 tautulli_api_token = ''
 
-import re
-
-if not re.search('^(\d{1,3}\.){3}\d{1,3}$', plex_ip):
-	print("Error: " + plex_ip + " is not a valid plex ip")
-	exit(1)
-
-if not re.search('^\d{1,5}$', plex_port):
-	print("Error: " + plex_port + " is not a valid plex port")
-	exit(1)
-
-if not re.search('^[\w\d_-~]{19,21}$', plex_api_token):
-	print("Error: " + plex_api_token + " is not a valid plex api token")
-	exit(1)
-
-if not re.search('^(\d{1,3}\.){3}\d{1,3}$', tautulli_ip):
-	print("Error: " + tautulli_ip + " is not a valid tautulli ip")
-	exit(1)
-
-if not re.search('^\d{1,5}$', tautulli_port):
-	print("Error: " + tautulli_port + " is not a valid tautulli port")
-	exit(1)
-
-if not re.search('^[0-9a-z]{32,34}$', tautulli_api_token):
-	print("Error: " + tautulli_api_token + " is not a valid tautulli api token")
-	exit(1)
-
+from os import getenv
 import requests
-import json
-import sys
-import getopt
 
-ssn = requests.Session()
-ssn.headers.update({'Accept': 'application/json'})
-ssn.params.update({'X-Plex-Token': plex_api_token})
-baseurl = 'http://' + plex_ip + ':' + plex_port
+# Environmental Variables
+plex_ip = getenv('plex_ip', plex_ip)
+plex_port = getenv('plex_port', plex_port)
+plex_api_token = getenv('plex_api_token', plex_api_token)
+plex_base_url = f"http://{plex_ip}:{plex_port}"
+tautulli_ip = getenv('tautulli_ip', tautulli_ip)
+tautulli_port = getenv('tautulli_port', tautulli_port)
+tautulli_api_token = getenv('tautulli_api_token', tautulli_api_token)
+tautulli_base_url = f"http://{tautulli_ip}:{tautulli_port}/api/v2"
 
-section_output = json.loads(ssn.get(baseurl + '/library/sections').text)
-rating_keys = []
-col_id = ''
-arguments, values = getopt.getopt(sys.argv[1:], 'L:', ['LibraryName='])
-lib_id = ''
-for argument, value in arguments:
-	if argument in ('-L', '--LibraryName'):
-		for level in section_output['MediaContainer']['Directory']:
-			if level['title'] == str(value): lib_id = level['key']
-		if not lib_id:
-			print('Error: library not found')
-			exit(1)
+def top10_collection(ssn, library_name: str, collection_title: str):
+	result_json = []
 
-if not lib_id:
-	print('Error: Required arguments were not all given\n-L/--LibraryName [Name of library to put the collection in]')
-	exit(1)
+	sections = ssn.get(f'{plex_base_url}/library/sections').json()['MediaContainer']['Directory']
+	#loop through the libraries
+	for lib in sections:
+		if lib['title'] == library_name:
+			#library found
+			lib_collections = ssn.get(f'{plex_base_url}/library/sections/{lib["key"]}/collections').json()['MediaContainer']
+			if 'Metadata' in lib_collections:
+				#delete old version of collection if present
+				for collection in lib_collections['Metadata']:
+					if collection['title'] == collection_title:
+						collection_id = collection['ratingKey']
+						ssn.delete(f'{plex_base_url}/library/collections/{collection_id}')
+						break
+			
+			#get new list of top movies
+			movies = requests.get(f'{tautulli_base_url}', params={'apikey': tautulli_api_token, 'cmd': 'get_home_stats', 'stat_id': 'top_movies'}).json()['response']['data']['rows']
+			movie_ratingkeys = [str(movie['rating_key']) for movie in movies]
+			result_json = movie_ratingkeys
+			
+			#create new collection
+			machine_id = ssn.get(f'{plex_base_url}/').json()['MediaContainer']['machineIdentifier']
+			ssn.post(f'{plex_base_url}/library/collections', params={'type': 1, 'title': collection_title, 'smart': 0, 'sectionId': lib['key'], 'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join(movie_ratingkeys)}'})
+			break
+	else:
+		return 'Library not found'
 
-for collection in json.loads(ssn.get(baseurl + '/library/sections/' + lib_id + '/collections').text)['MediaContainer']['Metadata']:
-	if collection['title'] == 'Top 10 movies':
-		col_id = collection['ratingKey']
+	return result_json
 
-if col_id: ssn.delete(baseurl + '/library/collections/' + col_id)
-for row in json.loads(requests.get('http://' + tautulli_ip + ':' + tautulli_port + '/api/v2', params={'apikey': tautulli_api_token, 'cmd': 'get_home_stats', 'stat_id': 'top_movies'}).text)['response']['data']['rows']:
-	rating_keys.append(str(row['rating_key']))
-machine_id = json.loads(ssn.get(baseurl + '/').text)['MediaContainer']['machineIdentifier']
-ssn.post(baseurl + '/library/collections', params={'type': 1, 'title': 'Top 10 movies', 'smart': 0, 'sectionId': lib_id, 'uri': 'server://' + machine_id + '/com.plexapp.plugins.library/library/metadata/' + ','.join(rating_keys)})
+if __name__ == '__main__':
+	import argparse
+
+	#setup vars
+	ssn = requests.Session()
+	ssn.headers.update({'Accept': 'application/json'})
+	ssn.params.update({'X-Plex-Token': plex_api_token})
+
+	#setup arg parsing
+	parser = argparse.ArgumentParser(description='Make a collection with the top 10 most popular movies according to tautulli')
+	parser.add_argument('-l','--LibraryName', type=str, help='Name of target library', required=True)
+	parser.add_argument('-t','--CollectionTitle', type=str, help='The title of the collection', default='Top 10 movies')
+
+	args = parser.parse_args()
+	#call function and process result
+	response = top10_collection(ssn=ssn, library_name=args.LibraryName, collection_title=args.CollectionTitle)
+	if not isinstance(response, list):
+		parser.error(response)
