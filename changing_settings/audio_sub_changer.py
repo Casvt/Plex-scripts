15bb5f7b25072378212837416702b27c5184e6e4
Cas van Tijn <casvantijn@gmail.com>
2022-05-11 15:02:01 +0000

    Fixed bug with movie_name

--- changing_settings/audio_sub_changer.py
+++ changing_settings/audio_sub_changer.py
@@ -125,7 +125,7 @@ def audio_sub_changer(ssn, type: str, language: str, library_name: str, movie_na
 				_set_track(ssn=ssn, user_tokens=user_tokens, rating_key=movie['ratingKey'], type=type, language=language)
 				result_json.append(movie['ratingKey'])
 
-				if movie_name != None:
+				if movie_name:
 					#the targeted movie was found and processed so exit loop
 					break
 
