6bbebf5a5ec86009a0c929af86f2649b6346ea8d
Cas van Tijn <casvantijn@gmail.com>
2022-05-11 14:30:40 +0000

    Improved importibility, effeciency and uniformity

--- various/poster_downloader.py
+++ various/poster_downloader.py
@@ -1,69 +1,153 @@
 #!/usr/bin/python3
+#-*- coding: utf-8 -*-
 
-#The use case of this script is the following:
-#	Download the poster of every entry in the library that you give to the destination foldr (.png)
+"""
+The use case of this script is the following:
+	Download the poster of the selected media into their media folder
+Requirements (python3 -m pip install [requirement]):
+	requests
+Setup:
+	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+"""
 
 plex_ip = ''
 plex_port = ''
 plex_api_token = ''
 
-import re
-import json
-import requests
-import sys
-import getopt
-import os
+from os import getenv
+from os.path import splitext
 
-if not re.search('^(\d{1,3}\.){3}\d{1,3}$', plex_ip):
-	print("Error: " + plex_ip + " is not a valid ip")
-	exit(1)
+# Environmental Variables
+plex_ip = getenv('plex_ip', plex_ip)
+plex_port = getenv('plex_port', plex_port)
+plex_api_token = getenv('plex_api_token', plex_api_token)
+base_url = f"http://{plex_ip}:{plex_port}"
 
-if not re.search('^\d{1,5}$', plex_port):
-	print("Error: " + plex_port + " is not a valid port")
-	exit(1)
+def _download_poster(ssn, media_info: dict):
+	file_path = ''
 
-if not re.search('^[\w\d_-~]{19,21}$', plex_api_token):
-	print("Error: " + plex_api_token + " is not a valid api token")
-	exit(1)
+	if 'thumb' in media_info and 'Media' in media_info:
+		poster = ssn.get(f'{base_url}{media_info["thumb"]}').content
+		file_path = splitext(media_info['Media'][0]['Part'][0]['file'])[0] + '.jpg'
+		with open(file_path, 'wb') as f:
+			f.write(poster)
 
-ssn = requests.Session()
-ssn.headers.update({'Accept': 'application/json'})
-ssn.params.update({'X-Plex-Token': plex_api_token})
-baseurl = 'http://' + plex_ip + ':' + plex_port
+	return file_path
 
-section_output = json.loads(ssn.get(baseurl + '/library/sections').text)
-arguments, values = getopt.getopt(sys.argv[1:], 'l:o:', ['LibraryName=', 'OutputPath='])
-lib_id = ''
-output_path = ''
-for argument, value in arguments:
-	if argument in ('-l', '--LibraryName'):
-		for level in section_output['MediaContainer']['Directory']:
-			if level['title'] == value:
-				if level['type'] == 'movie' or level['type'] == 'show':
-					lib_id = level['key']
-		if not lib_id:
-			print('Library not found or not a movie/show library')
-			exit(1)
-	if argument in ('-o', '--OutputPath'):
-		if os.path.exists(value): output_path = value
+def poster_downloader(ssn, library_name: str, movie_name: list=[], series_name: str=None, season_number: int=None, episode_number: int=None):
+	result_json = []
+
+	#check for illegal arg parsing
+	if season_number != None and series_name == None:
+		#season number given but no series name
+		return '"season_number" is set but not "series_name"'
+	if episode_number != None and (season_number == None or series_name == None):
+		#episode number given but no season number or no series name
+		return '"episode_number" is set but not "season_number" or "series_name"'
+
+	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer']['Directory']
+	#loop through the libraries
+	for lib in sections:
+		if lib['title'] != library_name: continue
+
+		#this library is targeted
+		print(lib['title'])
+		lib_output = ssn.get(f'{base_url}/library/sections/{lib["key"]}/all').json()['MediaContainer']['Metadata']
+		if lib['type'] == 'movie':
+			#library is a movie lib; loop through every movie
+			for movie in lib_output:
+				if movie_name and not movie['title'] in movie_name:
+					#a specific movie is targeted and this one is not it, so skip
+					continue
+
+				print(f'	{movie["title"]}')
+				result = _download_poster(ssn=ssn, media_info=movie)
+				if result: result_json.append(result)
+
+				if movie_name != None:
+					#the targeted movie was found and processed so exit loop
+					break
+
+		elif lib['type'] == 'show':
+			#library is show lib; loop through every show
+			for show in lib_output:
+				if series_name != None and show['title'] != series_name:
+					#a specific show is targeted and this one is not it, so skip
+					continue
+
+				print(f'	{show["title"]}')
+				show_output = ssn.get(f'{base_url}/library/metadata/{show["ratingKey"]}/allLeaves').json()['MediaContainer']['Metadata']
+				#loop through episodes of show to check if targeted season exists
+				if season_number != None:
+					for episode in show_output:
+						if episode['parentIndex'] == season_number:
+							break
+					else:
+						return 'Season not found'
+				#loop through episodes of show
+				for episode in show_output:
+					if season_number != None and episode['parentIndex'] != season_number:
+						#a specific season is targeted and this one is not it; so skip
+						continue
+
+					if episode_number != None and episode['index'] != episode_number:
+						#this season is targeted but this episode is not; so skip
+						continue
+
+					print(f'		S{episode["parentIndex"]}E{episode["index"]}	- {episode["title"]}')
+					result = _download_poster(ssn=ssn, media_info=episode)
+					if result: result_json.append(result)
+
+					if episode_number != None:
+						#the targeted episode was found and processed so exit loop
+						break
+				else:
+					if episode_number != None:
+						#the targeted episode was not found
+						return 'Episode not found'
+
+				if series_name != None:
+					#the targeted series was found and processed so exit loop
+					break
+			else:
+				if series_name != None:
+					#the targeted series was not found
+					return 'Series not found'
 		else:
-			print('Output path not found')
-			exit(1)
+			return 'Library not supported'
+		#the targeted library was found and processed so exit loop
+		break
+	else:
+		#the targeted library was not found
+		return 'Library not found'
 
-if not lib_id or not output_path:
-	print('Error: Arguments were not all given')
-	print('Required: -l/--LibraryName [name of target library], -o/--OutputPath [path to folder where posters are going to be put]')
-	exit(1)
+	return result_json
 
-lib_output = json.loads(ssn.get(baseurl + '/library/sections/' + lib_id + '/all').text)
-for level in lib_output['MediaContainer']['Metadata']:
-	#do this for every entry in the library
-	print(level['title'])
-	poster_output = json.loads(ssn.get(baseurl + '/library/metadata/' + level['ratingKey'] + '/posters').text)
-	for poster in poster_output['MediaContainer']['Metadata']:
-		#do this for every available poster of the entry
-		if poster['selected'] == True:
-			#download the poster to the OutputPath
-			file = open(os.path.join(output_path, level['title'] + '.png'), 'wb')
-			file.write(ssn.get(baseurl + poster['key']).content)
-			file.close()
+if __name__ == '__main__':
+	import requests, argparse
+
+	#setup vars
+	ssn = requests.Session()
+	ssn.headers.update({'Accept': 'application/json'})
+	ssn.params.update({'X-Plex-Token': plex_api_token})
+
+	#setup arg parsing
+	parser = argparse.ArgumentParser(description="Download the poster of the selected media into their media folder")
+	parser.add_argument('-l', '--LibraryName', type=str, help="Name of target library", required=True)
+	parser.add_argument('-m', '--MovieName', type=str, help="Target a specific movie inside a movie library based on it's name (only accepted when -l is a movie library); allowed to give argument multiple times", action='append', default=[])
+	parser.add_argument('-s', '--SeriesName', type=str, help="Target a specific series inside a show library based on it's name (only accepted when -l is a show library)")
+	parser.add_argument('-S', '--SeasonNumber', type=int, help="Target a specific season inside the targeted series based on it's number (only accepted when -s is given) (specials is 0)")
+	parser.add_argument('-e', '--EpisodeNumber', type=int, help="Target a specific episode inside the targeted season based on it's number (only accepted when -S is given)")
+
+	args = parser.parse_args()
+	#call function and process result
+	response = poster_downloader(ssn=ssn, library_name=args.LibraryName, movie_name=args.MovieName, series_name=args.SeriesName, season_number=args.SeasonNumber, episode_number=args.EpisodeNumber)
+	if not isinstance(response, list):
+		if response == '"season_number" is set but not "series_name"':
+			parser.error('-S/--SeasonNumber given but not -s/--SeriesName given')
+
+		elif response == '"episode_number" is set but not "season_number" or "series_name"':
+			parser.error('-e/--EpisodeNumber given but -S/--SeasonNumber or -s/--SeriesName not given')
+
+		else:
+			parser.error(response)
