a5ad79033b81c2ed20f80d619241f32f6246299c
Cas van Tijn <casvantijn@gmail.com>
2022-05-11 14:13:13 +0000

    Improved importibility and uniformity

--- various/thanos_snap.py
+++ various/thanos_snap.py
@@ -1,29 +1,39 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+"""
+The use case of this script is the following:
+	A 50/50 chance that a command will be execute exactly when Thanos snaps it's fingers in Avangers: Infinity War
+Requirements (python3 -m pip install [requirement]):
+	NO REQUIREMENTS
+Setup:
+	Fill the variables below firstly, then go to the tautulli web-ui -> Settings -> Notification Agents -> Add a new notification agent -> Script:
+		Configuration:
+			Script Folder = /path/to/script/folder
+			Script File = select this script
+			Script Timeout = 0
+			Description = whatever you want
+		Triggers:
+			Playback Start = check
+		Conditions:
+			Condition {1} = Progress Duration (sec) is 7866
+			Condition {2} = Title is Avengers: Infinity War
+			Condition Logic = {1} and {2}
+Warning:
+	THERE'S A 50/50 CHANCE THAT WHEN THANOS SNAPS IT'S FINGERS,
+	A COMMAND IS EXECUTED THAT COULD LEAD TO DESTRUCTION OF FILES/DIRECTORIES/ETC.
+	I AM NOT RESPONSIBLE FOR ANY LOSS OF IMPORTANT DATA
+"""
+
 import random
 
-#The use case of this script is the following:
-#   When you're watching Avengers: Infinity War, at the moment that thanos snaps, this script will be launched.
-#   When the script is launched, there is a 50/50 chance that a command to your liking is executed
-#
-#SETUP:
-#In Tautulli, go to Settings -> Notification Agents -> Add a new notification agent -> Script
-#	Configuration:
-#		Script Folder = folder where this script is stored
-#		Script File = select this script
-#		Script Timeout = 0
-#		Description is optional
-#	Triggers:
-#		Playback Start = check
-#   Conditions:
-#       Condition {1} = Progress Duration (sec) is 7866
-#       Condition {2} = Title is Avengers: Infinity War
-#       Condition Logic = {1} and {2}
-#SAVE
-#
-#WARNING: THERE'S A 50/50 CHANCE THAT A COMMAND IS EXECUTED THAT COULD LEAD TO DESTRUCTION OF FILES/DIRECTORIES
-#         I AM NOT RESPONSIBLE FOR ANY LOSS OF IMPORTANT DATA
+def thanos_snap():
+	if random.randrange(1,3) == 2:
+		print('It\'s not your lucky day')
+		#place destructive command here
+	else:
+		print('It\s your lucky day')
 
-if random.randrange(1,3) == 2:
-	print('It\'s not your lucky day')
-	#place destructive command here
-else:
-	print('It\s your lucky day')
+if __name__ == '__main__':
+	#call function
+	thanos_snap()
