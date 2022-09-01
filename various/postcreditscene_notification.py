d699fa0f5fef058c66edda1a642a120b8dc5e984
CasVT <casvantijn@gmail.com>
2022-09-01 17:35:16 +0200

    Search for original title first

--- various/postcreditscene_notification.py
+++ various/postcreditscene_notification.py
@@ -25,10 +25,13 @@ Setup:
 			-- Operator -- = is
 			-- Value -- = your username
 		Arguments:
-			Playback Start -> Script Arguments = --Title {title} --Year {year}
+			Playback Start -> Script Arguments = --RatingKey {rating_key}
 """
 
 apprise_url = ''
+plex_ip = ''
+plex_port = ''
+plex_api_token = ''
 
 from os import getenv
 from requests import get as requests_get
@@ -37,16 +40,22 @@ from apprise import Apprise
 
 # Environment Variables
 apprise_url = getenv('apprise_url', apprise_url)
+plex_ip = getenv('plex_ip', plex_ip)
+plex_port = getenv('plex_port', plex_port)
+plex_api_token = getenv('plex_api_token', plex_api_token)
+base_url = f'http://{plex_ip}:{plex_port}'
 
-def postcreditscene_notification(title: str, year: str):
+def postcreditscene_notification(rating_key: str):
 	result_json = []
 
+	media_info = requests_get(f'{base_url}/library/metadata/{rating_key}', params={'X-Plex-Token': plex_api_token}, headers={'Accept': 'application/json'}).json()['MediaContainer']['Metadata'][0]
+	title, year = media_info.get('originalTitle', media_info.get('title','')), media_info.get('year',0)
 	search_results = requests_get(f'https://aftercredits.com/', params={'s': f'{title} {year}'}, headers={'Host': 'aftercredits.com','Referer': f'https://aftercredits.com/?s={title}+{year}'.replace(' ','+'), 'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"'}).text
 	result = search(f'(?i)>(stingers|after credits)</a>            </div>\\r\\n            <h3 class=\"entry-title td-module-title\"><a href=\"[^\"]+?\" rel=\"bookmark\" title=\"{title} \({year}\)\*', search_results)
 	if result:
 		#media has post-credit scene
 		print(f'{title} ({year}) has a post-credit scene')
-		result_json.append(title)
+		result_json.append(media_info['title'])
 
 		app = Apprise()
 		app.add(apprise_url)
@@ -62,11 +71,10 @@ if __name__ == '__main__':
 
 	#setup arg parsing
 	parser = ArgumentParser(description='Get a notification when the media being watched has a post-credit scene')
-	parser.add_argument('-t','--Title', type=str, help='The title of the media to check', required=True)
-	parser.add_argument('-y','--Year', type=str, help='The release year of the media to check', required=True)
+	parser.add_argument('-k','--RatingKey', type=str, help='The rating key of the media to check', required=True)
 
 	args = parser.parse_args()
 	#call function and process result
-	response = postcreditscene_notification(title=args.Title, year=args.Year)
+	response = postcreditscene_notification(rating_key=args.RatingKey)
 	if not isinstance(response, list):
 		parser.error(response)
