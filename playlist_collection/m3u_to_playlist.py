263f2b7e326404f602f3460d5f3dc0f97a92f753
CasVT <casvantijn@gmail.com>
2023-05-02 14:21:13 +0200

    Added multi user support

--- playlist_collection/m3u_to_playlist.py
+++ playlist_collection/m3u_to_playlist.py
@@ -23,36 +23,49 @@ plex_port = getenv('plex_port', plex_port)
 plex_api_token = getenv('plex_api_token', plex_api_token)
 base_url = f"http://{plex_ip}:{plex_port}"
 
-def m3u_to_playlist(ssn, library_name: str, file_path: str):
-	#check for illegal arg parsing
+def m3u_to_playlist(ssn, library_name: str, file_path: str, users: list=['@me']):
+	# Check for illegal arg parsing
 	if not path.isfile(file_path):
 		return 'File not found'
 
-	#loop through the libraries
-	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer']['Directory']
+	# Get tokens of users
+	machine_id = ssn.get(f'{base_url}/').json()['MediaContainer']['machineIdentifier']
+	shared_users = ssn.get(f'http://plex.tv/api/servers/{machine_id}/shared_servers').text
+	result = map(lambda r: r.split('"')[0:7:6], shared_users.split('username="')[1:])
+	user_data = dict(result)
+	user_data['@me'] = plex_api_token
+	if not '@all' in users:
+		for username in user_data.keys():
+			if not username in users:
+				user_data.pop(username)
+
+	# Loop through the libraries
+	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer'].get('Directory', [])
 	for lib in sections:
 		if lib['title'] == library_name:
-			ssn.post(f'{base_url}/playlists/upload', params={'sectionID': lib['key'], 'path': file_path})
-			break
-	else:
-		return 'Library not found'
-	return
+			for user_token in user_data.values():
+				ssn.post(f'{base_url}/playlists/upload', params={'sectionID': lib['key'], 'path': file_path, 'X-Plex-Token': user_token})
+			return
+
+	return 'Library not found'
 
 if __name__ == '__main__':
-	import requests, argparse
+	from requests import Session
+	from argparse import ArgumentParser
 
-	#setup vars
-	ssn = requests.Session()
+	# Setup vars
+	ssn = Session()
 	ssn.headers.update({'Accept': 'application/json'})
 	ssn.params.update({'X-Plex-Token': plex_api_token})
 
-	#setup arg parsing
-	parser = argparse.ArgumentParser(description='Set the first image of an album as the album cover')
+	# Setup arg parsing
+	parser = ArgumentParser(description='Convert a .m3u file to a plex playlist')
 	parser.add_argument('-l','--LibraryName', type=str, help='Name of target library', required=True)
 	parser.add_argument('-f','--File', type=str, help="File path to the .m3u file", required=True)
+	parser.add_argument('-u','--User', help='Apply user-specific sync actions to these users; This argument can be given multiple times; Use @me to target yourself; Use @all to target everyone', action='append', default=['@me'])
 
 	args = parser.parse_args()
-	#call function and process result
-	response = m3u_to_playlist(ssn=ssn, library_name=args.LibraryName, file_path=args.File)
-	if not response == None:
+	# Call function and process result
+	response = m3u_to_playlist(ssn=ssn, library_name=args.LibraryName, file_path=args.File, users=args.User)
+	if response is not None:
 		parser.error(response)
