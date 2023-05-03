f8d6b7d0f23cf6598bd52c6916a54dd855a27474
CasVT <casvantijn@gmail.com>
2023-05-03 16:30:57 +0200

    Refactoring

--- changing_settings/plex_auto_delete.py
+++ changing_settings/plex_auto_delete.py
@@ -15,6 +15,7 @@ plex_port = ''
 plex_api_token = ''
 
 from os import getenv
+from typing import List
 
 # Environmental Variables
 plex_ip = getenv('plex_ip', plex_ip)
@@ -22,56 +23,57 @@ plex_port = getenv('plex_port', plex_port)
 plex_api_token = getenv('plex_api_token', plex_api_token)
 base_url = f"http://{plex_ip}:{plex_port}"
 
-def plex_auto_delete(ssn, value: str, library_name: str, series_name: list=[]):
+value_mapping = {
+	'never': '0',
+	'after_day': '1',
+	'after_week': '7'
+}
+
+def plex_auto_delete(ssn, value: str, library_name: str, series_names: List[str]=[]) -> List[int]:
 	result_json = []
 
-	#check for illegal arg parsing
-	if not value in ('never','after_day','after_week'):
+	# Setup vars
+	value_id = value_mapping.get(value)
+	if not value_id:
 		return 'Invalid value for "value"'
 
-	#setup vars
-	if value == 'never': value_id = '0'
-	elif value == 'after_day': value_id = '1'
-	elif value == 'after_week': value_id = '7'
-
-	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer']['Directory']
-	#loop through the libraries
+	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer'].get('Directory', [])
 	for lib in sections:
 		if lib['title'] != library_name: continue
 		if lib['type'] != 'show': return 'Invalid library'
 
-		#this library is targeted
 		print(lib['title'])
-		lib_output = ssn.get(f'{base_url}/library/sections/{lib["key"]}/all').json()['MediaContainer']['Metadata']
-		#loop through the shows in the library
+		lib_output = ssn.get(f'{base_url}/library/sections/{lib["key"]}/all').json()['MediaContainer'].get('Metadata', [])
 		for show in lib_output:
-			if series_name and not show['title'] in series_name:
-				#a specific show is targeted and this one is not it, so skip
+			if series_names and not show['title'] in series_names:
 				continue
 
 			print(f'	{show["title"]}')
-			#set value of setting
-			ssn.put(f'{base_url}/library/metadata/{show["ratingKey"]}/prefs', params={'autoDeletionItemPolicyWatchedLibrary': value_id})
+			ssn.put(
+				f'{base_url}/library/metadata/{show["ratingKey"]}/prefs',
+	   			params={'autoDeletionItemPolicyWatchedLibrary': value_id}
+			)
 			result_json.append(show['ratingKey'])
 
 	return result_json
 
 if __name__ == '__main__':
-	import requests, argparse
+	from requests import Session
+	from argparse import ArgumentParser
 
-	#setup vars
-	ssn = requests.Session()
+	# Setup vars
+	ssn = Session()
 	ssn.headers.update({'Accept': 'application/json'})
 	ssn.params.update({'X-Plex-Token': plex_api_token})
 
-	#setup arg parsing
-	parser = argparse.ArgumentParser(description="Set the value of the shows setting \"auto delete after watching\" for all the selected shows")
-	parser.add_argument('-v', '--Value', choices=['never','after_day','after_week'], help="The value that the setting should be set to", required=True)
+	# Setup arg parsing
+	parser = ArgumentParser(description="Set the value of the shows setting \"auto delete after watching\" for all the selected shows")
+	parser.add_argument('-v', '--Value', choices=value_mapping.keys(), help="The value that the setting should be set to", required=True)
 	parser.add_argument('-l', '--LibraryName', type=str, help="Name of target show library", required=True)
 	parser.add_argument('-s', '--SeriesName', help="Target a specific series inside a show library based on it's name; This argument can be given multiple times to target multiple series", action='append', default=[])
 
 	args = parser.parse_args()
-	#call function and process result
-	response = plex_auto_delete(ssn=ssn, value=args.Value, library_name=args.LibraryName, series_name=args.SeriesName)
+	# Call function and process result
+	response = plex_auto_delete(ssn=ssn, value=args.Value, library_name=args.LibraryName, series_names=args.SeriesName)
 	if not isinstance(response, list):
 		parser.error(response)
