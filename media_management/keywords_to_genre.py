be2a010bb39f0698319e49d1d728e8018b2524e4
CasVT <casvantijn@gmail.com>
2024-04-16 16:07:52 +0200

    Refactored/Updated keywords_to_genre.py (Fixes #198) (Resolves #199)

--- media_management/keywords_to_genre.py
+++ media_management/keywords_to_genre.py
@@ -1,69 +1,106 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	Add a keyword to the genre list in plex if the keyword is present in the keyword list of the media on the IMDB
+	Add a keyword to the genre/label list in plex if the keyword is present in
+	the keyword list for the media on the IMDB.
+
 Requirements (python3 -m pip install [requirement]):
 	requests
+
 Setup:
-	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+	1. Fill the variables below.
+	2. Run the script in a terminal/shell with the "-h" flag to learn more about the parameters.
+		python3 keywords_to_genre.py -h
+		or
+		python keywords_to_genre.py -h
 """
 
-plex_ip = ''
-plex_port = ''
+#===== FILL THESE VARIABLES =====
+plex_base_url = ''
 plex_api_token = ''
+#================================
 
+from fnmatch import filter as fnmatch_filter
+from json import loads
 from os import getenv
 from re import compile
-from fnmatch import filter as fnmatch_filter
 from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
 	from requests import Session
 
 # Environmental Variables
-plex_ip = getenv('plex_ip', plex_ip)
-plex_port = getenv('plex_port', plex_port)
+plex_base_url = getenv('plex_base_url', plex_base_url)
 plex_api_token = getenv('plex_api_token', plex_api_token)
-base_url = f"http://{plex_ip}:{plex_port}"
-keyword_finder = compile(r'(?<=data-item-keyword=").*?(?=")')
+base_url = plex_base_url.rstrip('/')
+json_finder = compile(r'(?<=script id="__NEXT_DATA__" type="application/json">).*?(?=</script>)')
 
 def keywords_to_genre(
 	ssn: 'Session',
 	keywords: List[str],
 	library_names: List[str],
-	movie_names: List[str]=[],
-	series_names: List[str]=[],
-	skip_locked: bool=False
+	movie_names: List[str] = [],
+	series_names: List[str] = [],
+	skip_locked: bool = False,
+	use_label: bool = False
 ) -> List[int]:
-	result_json = []
+	"""Add a keyword to the genre/label list in plex if the keyword is present in
+	the keyword list for the media on the IMDB.
 
-	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer'].get('Directory', [])
+	Args:
+		ssn (Session): The plex requests session to fetch with.
+		keywords (List[str]): The keywords to search for.
+		library_names (List[str]): The names of the libraries to cover.
+		movie_names (List[str], optional): The movie names to cover. Defaults to [].
+		series_names (List[str], optional): The series names to cover. Defaults to [].
+		skip_locked (bool, optional): Skip media if field is locked. Defaults to False.
+		use_label (bool, optional): Add to keywords to labels instead of genres. Defaults to False.
+
+	Returns:
+		List[int]: List of media rating keys that were processed.
+	"""
+	result_json = []
+	sections = ssn.get(
+		f'{base_url}/library/sections'
+	).json()['MediaContainer'].get('Directory', [])
+	field = 'genre' if not use_label else 'label'
+
 	for lib in sections:
-		if not lib['title'] in library_names:
+		if not (
+			lib['type'] in ('movie', 'show')
+			and lib['title'] in library_names
+		):
 			continue
-		
-		# Check for illegal arg parsing
-		if not lib['type'] in ('movie','show'): return 'Invalid library type'
-		
+
 		lib_output: List[dict] = ssn.get(
 			f'{base_url}/library/sections/{lib["key"]}/all',
 			params={'includeGuids': '1'}
 		).json()['MediaContainer'].get('Metadata', [])
 
 		for media in lib_output:
-			if ((lib['type'] == 'movie' and movie_names and not media['title'] in movie_names)
-       		or (lib['type'] == 'show' and series_names and not media['title'] in series_names)):
+			if (
+				lib['type'] == 'movie'
+				and movie_names
+				and not media['title'] in movie_names
+       		or
+				lib['type'] == 'show'
+				and series_names
+				and not media['title'] in series_names
+			):
 				continue
-			
+
 			media_output: dict = ssn.get(
 				f'{base_url}/library/metadata/{media["ratingKey"]}'
-			).json()['MediaContainer']['Metadata'][0]
-			
-			# Skip media if genre field is locked and skip_locked is True
+			).json()['MediaContainer'].get('Metadata', (None,))[0]
+
+			if not media_output:
+				continue
+
+			# Skip media if field is locked and skip_locked is True
 			if skip_locked:
-				if any(l['name'] == 'genre' and l['locked'] for l in media_output.get('Field', [])):
+				if any(l['name'] == field and l['locked'] for l in media_output.get('Field', [])):
 					continue
 
 			# Find IMDB id of media otherwise skip
@@ -73,39 +110,47 @@ def keywords_to_genre(
 					break
 			else:
 				continue
-			
-			# Get all genres that media has been tagged with
-			media_genres = [g['tag'] for g in media_output.get('Genre', [])]
-			
+
+			# Get all genres/labels that media has been tagged with
+			current_values = [g['tag'] for g in media_output.get(field.capitalize(), [])]
+
 			# Get all keywords that media has on IMDB
-			imdb_info = ssn.get(f'https://www.imdb.com/title/{media_guid}/keywords').text
-			media_keywords = keyword_finder.findall(imdb_info)
+			imdb_info = ssn.get(
+				f'https://www.imdb.com/title/{media_guid}/keywords',
+				headers={'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36'}
+			).text
+			re_result = json_finder.search(imdb_info)
+			if not re_result:
+				continue
+			imdb_json = loads(re_result.group(0))
+			media_keywords = [e['rowTitle'] for e in imdb_json['props']['pageProps']['contentData']['section']['items'] if e['userVotingProps']['itemType'] == 'TitleKeyword']
 
 			# Find keywords that are desired to be added but aren't already
-			new_genres = []
+			new_values = []
 			for k in keywords:
 				if '*' in k:
 					result = fnmatch_filter(media_keywords, k)
-					new_genres += [r for r in result if not r in media_genres]
+					new_values += [r for r in result if not r in current_values]
 				else:
-					if k in media_keywords and not k in media_genres:
-						new_genres.append(k)
-						
-			# Push new genres
+					if k in media_keywords and not k in current_values:
+						new_values.append(k)
+
+			# Push new values
 			payload = {
 				'type': '2' if media['type'] == 'show' else '1',
 				'id': media['ratingKey'],
-				'genre.locked': '1',
-				**{f'genre[{c}].tag.tag': v for c, v in enumerate(media_genres + new_genres)}
+				f'{field}.locked': '1',
+				**{f'{field}[{c}].tag.tag': v for c, v in enumerate(current_values + new_values)}
 			}
 			ssn.put(f'{base_url}/library/sections/{lib["key"]}/all', params=payload)
-			
+
 			result_json.append(media['ratingKey'])
 	return result_json
 
 if __name__ == '__main__':
-	from requests import Session
 	from argparse import ArgumentParser
+
+	from requests import Session
 
 	# Setup vars
 	ssn = Session()
@@ -113,26 +158,21 @@ if __name__ == '__main__':
 	ssn.params.update({'X-Plex-Token': plex_api_token})
 
 	# Setup arg parsing
-	parser = ArgumentParser(description='Add a keyword to the genre list in plex if the keyword is present in the keyword list of the media on the IMDB')
-	parser.add_argument('-k','--Keyword', type=str, help='Keyword that will be added to genre list if found in keyword list; allowed to give multiple times; supports wildcards (*)', action='append', required=True)
-	parser.add_argument('-l','--LibraryName', type=str, help='Name of target library; allowed to give multiple times', action='append', required=True)
-	parser.add_argument('-m','--MovieName', type=str, help='Name of target movie; allowed to give multiple times', action='append', default=[])
-	parser.add_argument('-s','--SeriesName', type=str, help='Name of target series; allowed to give multiple times', action='append', default=[])
-	parser.add_argument('-S','--SkipLocked', help='Skip media that has it\'s genre field locked', action='store_true')
+	parser = ArgumentParser(description="Add a keyword to the genre/label list in plex if the keyword is present in the keyword list for the media on the IMDB.")
+	parser.add_argument('-k', '--Keyword', type=str, action='append', required=True, help='Keyword that will be added if found in keyword list; allowed to give multiple times; supports wildcards (*)')
+	parser.add_argument('-l', '--LibraryName', type=str, action='append', required=True, help='Name of target library; allowed to give multiple times')
+	parser.add_argument('-m', '--MovieName', type=str, action='append', default=[], help='Name of target movie; allowed to give multiple times')
+	parser.add_argument('-s', '--SeriesName', type=str, action='append', default=[], help='Name of target series; allowed to give multiple times')
+	parser.add_argument('-S', '--SkipLocked', action='store_true', help="Skip media that has it's field locked")
+	parser.add_argument('-L', '--UseLabel', action='store_true', help="Put keywords in label fields instead of genre field")
 
 	args = parser.parse_args()
-	# Call function and process result
-	response = keywords_to_genre(
-		ssn=ssn, keywords=args.Keyword,
+
+	keywords_to_genre(
+		ssn, keywords=args.Keyword,
 		library_names=args.LibraryName,
 		movie_names=args.MovieName,
 		series_names=args.SeriesName,
-		skip_locked=args.SkipLocked
+		skip_locked=args.SkipLocked,
+		use_label=args.UseLabel
 	)
-	if not isinstance(response, list):
-		if response == 'Library is a movie library but "series_names" is set':
-			parser.error('Library is a movie library but -s/--SeriesName is set')
-		elif response == 'Library is a show library but "movie_names" is set':
-			parser.error('Library is a show library but -m/--MovieName is set')
-		else:
-			parser.error(response)
