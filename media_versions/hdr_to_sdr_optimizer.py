c16b94c57ec473457a8b4869503f77ea4628f374
CasVT <casvantijn@gmail.com>
2024-07-15 14:41:46 +0200

    Refactored hdr_to_sdr_optimizer.py

--- media_versions/hdr_to_sdr_optimizer.py
+++ media_versions/hdr_to_sdr_optimizer.py
@@ -1,158 +1,311 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	Optimize HDR media, found in the libraries given, when it isn't already available in SDR
+    Optimize HDR media when it isn't already available in SDR.
+
 Requirements (python3 -m pip install [requirement]):
-	requests
-	PlexAPI
+    requests
+    PlexAPI
+
 Setup:
-	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
-To-Do:
-	1. Don't add media to the optimize-queue when it's already there
+    1. Fill the variables below.
+    2. Run the script in a terminal/shell with the "-h" flag to learn more about the parameters.
+        python3 hdr_to_sdr_optimizer.py -h
+        or
+        python hdr_to_sdr_optimizer.py -h
+
+Examples:
+    --Limit 20 --AllMovie
+
+        Optimize the first 20 movies that don't have an SDR version yet.
+
+    --Library "Tv-series" --SeriesName "Breaking Bad"
+
+        Create an SDR version of all episodes of "Breaking Bad" in the "Tv-series" library.
 """
 
-plex_ip = ''
-plex_port = ''
-plex_api_token = ''
-
+from dataclasses import dataclass, field
 from os import getenv
+from typing import TYPE_CHECKING, Any, Dict, Generator, List
+
+if TYPE_CHECKING:
+    from plexapi.server import PlexServer
+    from requests import Session
+
+# ===== FILL THESE VARIABLES =====
+plex_base_url = ''
+plex_api_token = ''
+# ================================
 
 # Environmental Variables
-plex_ip = getenv('plex_ip', plex_ip)
-plex_port = getenv('plex_port', plex_port)
+plex_base_url = getenv('plex_base_url', plex_base_url)
 plex_api_token = getenv('plex_api_token', plex_api_token)
-base_url = f"http://{plex_ip}:{plex_port}"
+base_url = plex_base_url.rstrip('/')
 
-def _check_media_hdr(ssn, plex, media_ratingkey: str):
-	media_info = ssn.get(f'{base_url}/library/metadata/{media_ratingkey}').json()['MediaContainer']['Metadata'][0]
-	for media in media_info['Media']:
-		if 'title' in media and media['title'].startswith('Optimized for '):
-			break
-	else:
-		plex.fetchItem(int(media_ratingkey)).optimize(locationID=-1, targetTagID=2, deviceProfile='Universal TV')
-		return 'Converting'
 
-	return 'Not-Converting'
+@dataclass
+class LibraryFilter:
+    all: bool = False
+    all_movie: bool = False
+    all_show: bool = False
+    libraries: List[str] = field(default_factory=lambda: [])
+    movies: List[str] = field(default_factory=lambda: [])
+    series: List[str] = field(default_factory=lambda: [])
+    season_numbers: List[int] = field(default_factory=lambda: [])
+    episode_numbers: List[int] = field(default_factory=lambda: [])
 
-def hdr_to_sdr_optimizer(ssn, plex, library_name: str, movie_name: list=[], series_name: str=None, season_number: int=None, episode_number: int=None, limit: int=None):
-	result_json = []
-	counter = 0
+    def __post_init__(self):
+        self.content_specifiers = (
+            self.libraries,
+            self.movies,
+            self.series, self.season_numbers, self.episode_numbers
+        )
+        self.lib_specifiers = (
+            self.all_movie, self.all_show
+        )
 
-	#check for illegal arg parsing
-	if season_number != None and series_name == None:
-		#season number given but no series name
-		return '"season_number" is set but not "series_name"'
-	if episode_number != None and (season_number == None or series_name == None):
-		#episode number given but no season number or no series name
-		return '"episode_number" is set but not "season_number" or "series_name"'
+        if self.all:
+            if (
+                any(self.content_specifiers)
+                or True in self.lib_specifiers
+            ):
+                raise ValueError(
+                    "Can't combine the 'all' target specifier with any other target specifier")
 
-	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer']['Directory']
-	#loop through the libraries
-	for lib in sections:
-		if lib['title'] != library_name: continue
+        else:
+            if True not in self.lib_specifiers and not self.libraries:
+                raise ValueError(
+                    "Either have to select all libraries of a type or supply library names")
 
-		#this library is targeted
-		print(lib['title'])
-		if lib['type'] == 'movie':
-			#library is a movie lib; loop through every HDR movie
-			lib_output = ssn.get(f'{base_url}/library/sections/{lib["key"]}/all', params={'hdr': '1'}).json()['MediaContainer']
-			if not 'Metadata' in lib_output:
-				#no hdr media in the library
-				break
-			for movie in lib_output['Metadata']:
-				if limit != None and counter == limit:
-					print('Limit reached')
-					return result_json
+            if len(self.series) > 1:
+                if self.season_numbers:
+                    # Season numbers with multiple series
+                    raise ValueError(
+                        "Can't give season numbers for multiple series")
 
-				if movie_name and not movie['title'] in movie_name:
-					#a specific movie is targeted and this one is not it, so skip
-					continue
+                elif self.episode_numbers:
+                    # Episode numbers with multiple series
+                    raise ValueError(
+                        "Can't give episode numbers for multiple series")
 
-				print(f'	{movie["title"]}')
-				counter += 1
-				result = _check_media_hdr(ssn, plex, movie['ratingKey'])
-				if result == 'Converting':
-					print('		Optimizing')
-				result_json.append(movie['ratingKey'])
+            elif len(self.series) == 1:
+                if self.episode_numbers:
+                    if not self.season_numbers:
+                        # Episode numbers without a season
+                        raise ValueError(
+                            "Can't give episode numbers without specifying a season number")
 
-		elif lib['type'] == 'show':
-			#library is show lib; loop through every HDR episode
-			lib_output = ssn.get(f'{base_url}/library/sections/{lib["key"]}/all', params={'episode.hdr': '1', 'type': '4'}).json()['MediaContainer']
-			if not 'Metadata' in lib_output:
-				#no hdr media in the library
-				break
-			for episode in lib_output['Metadata']:
-				if limit != None and counter == limit:
-					print('Limit reached')
-					return result_json
+                    elif len(self.season_numbers) > 1:
+                        # Episode numbers with multiple seasons
+                        raise ValueError(
+                            "Can't give episode numbers with multiple seasons")
 
-				if series_name != None and episode['grandparentTitle'] != series_name:
-					#a specific show is targeted and this one is not it, so skip
-					continue
+            else:
+                # No series specified
+                if self.season_numbers:
+                    # Season numbers but no series
+                    raise ValueError(
+                        "Can't give season numbers without specifying a series")
 
-				if season_number != None and episode['parentIndex'] != season_number:
-					#a specific season is targeted and this one is not it, so skip
-					continue
+                elif self.episode_numbers:
+                    # Episode numbers but no series
+                    raise ValueError(
+                        "Can't give episode numbers without specifying a series")
 
-				if episode_number != None and episode['index'] != episode_number:
-					#a specific episode is targeted and this one is not it, so skip
-					continue
+        return
 
-				print(f'	{episode["grandparentTitle"]}	- S{episode["parentIndex"]}E{episode["index"]}	- {episode["title"]}')
-				counter += 1
-				result = _check_media_hdr(ssn, plex, episode['ratingKey'])
-				if result == 'Converting':
-					print('		Optimizing')
-				result_json.append(episode['ratingKey'])
 
-				if episode_number != None:
-					#the targeted episode was found and processed so exit loop
-					break
-			else:
-				if episode_number != None:
-					#the targeted episode was not found
-					return 'Episode not found'
+def _get_library_entries(
+    ssn: 'Session',
+    library_filter: LibraryFilter
+) -> Generator[Dict[str, Any], Any, Any]:
+    """Get library entries to iterate over.
 
-		else:
-			return 'Library not supported'
-		#the targeted library was found and processed so exit loop
-		break
-	else:
-		#the targeted library was not found
-		return 'Library not found'
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        library_filter (LibraryFilter): The filters to apply to the media.
 
-	return result_json
+    Yields:
+        Generator[Dict[str, Any], Any, Any]: The resulting media information.
+    """
+    lf = library_filter
+
+    sections: List[dict] = ssn.get(
+        f'{base_url}/library/sections'
+    ).json()['MediaContainer'].get('Directory', [])
+
+    for lib in sections:
+        if not (
+            lib['type'] in ('movie', 'show')
+            and
+                lf.all
+                or lf.all_movie and lib['type'] == 'movie'
+                or lf.all_show and lib['type'] == 'show'
+                or lf.libraries and lib['title'] in lf.libraries
+        ):
+            continue
+
+        print(lib['title'])
+
+        if lib['type'] == 'movie':
+            params = {"hdr": "1"}
+        else:
+            params = {"episode.hdr": 1, "type": 4}
+
+        lib_output: List[dict] = ssn.get(
+            f'{base_url}/library/sections/{lib["key"]}/all',
+            params=params
+        ).json()['MediaContainer'].get('Metadata', [])
+
+        if lib['type'] == 'movie':
+            for movie in lib_output:
+                if lf.movies and not movie['title'] in lf.movies:
+                    continue
+
+                print(f'    {movie["title"]}')
+                yield movie
+
+        elif lib['type'] == 'show':
+            for episode in lib_output:
+                if lf.series and not episode['grandparentTitle'] in lf.series:
+                    continue
+                if (
+                    lf.season_numbers
+                    and not episode['parentIndex'] in lf.season_numbers
+                ):
+                    continue
+
+                if (
+                    lf.episode_numbers
+                    and not episode['index'] in lf.episode_numbers
+                ):
+                    continue
+
+                print(
+                    f'    {episode["grandparentTitle"]} - S{episode["parentIndex"]}E{episode["index"]}')
+                yield episode
+
+    return
+
+
+def _optimize_check(
+    ssn: 'Session',
+    media_info: Dict[str, Any]
+) -> bool:
+    """Check if a certain media has been optimized with a profile.
+
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        media_info (Dict[str, Any]): The info of the media to check.
+
+    Returns:
+        bool: Whether or not the media has an SDR version.
+    """
+    extra_info: Dict[str, Any] = ssn.get(
+        f'{base_url}/library/metadata/{media_info["ratingKey"]}'
+    ).json()["MediaContainer"]["Metadata"][0]
+
+    for media in extra_info["Media"]:
+        if media.get("title", '').startswith("Optimized for "):
+            return True
+    return False
+
+
+def auto_optimize(
+    ssn: 'Session',
+    plex: 'PlexServer',
+    library_filter: LibraryFilter,
+    limit: int = -1
+) -> List[int]:
+    """Optimize the targeted media if it isn't already.
+
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        plex (PlexServer): The plex instance to optimize with.
+        library_filter (LibraryFilter): The filter to apply to the media.
+        limit (int, optional): The max amount of media to optimize in one run.
+            Defaults to -1.
+
+    Raises:
+        ValueError: Invalid profile.
+
+    Returns:
+        List[int]: List of media rating keys that were processed.
+    """
+    result_json = []
+    counter = 0
+
+    for media in _get_library_entries(ssn, library_filter):
+        if limit >= 0 and counter == limit:
+            print('Limit reached')
+            return result_json
+
+        if _optimize_check(ssn, media):
+            continue
+
+        print(f'        Creating SDR version')
+
+        plex.fetchItem(int(media['ratingKey'])).optimize(
+            locationID=-1,
+            target="tv"
+        )
+        result_json.append(media['ratingKey'])
+        counter += 1
+
+    return result_json
+
 
 if __name__ == '__main__':
-	import requests, argparse
-	from plexapi.server import PlexServer
+    from argparse import ArgumentParser
 
-	#setup vars
-	ssn = requests.Session()
-	ssn.headers.update({'Accept': 'application/json'})
-	ssn.params.update({'X-Plex-Token': plex_api_token})
-	plex = PlexServer(base_url, plex_api_token)
+    from plexapi.server import PlexServer
+    from requests import Session
 
-	#setup arg parsing
-	parser = argparse.ArgumentParser(description="Optimize HDR media, found in the libraries given, when it isn't already available in SDR")
-	parser.add_argument('-l', '--LibraryName', type=str, help="Name of target library", required=True)
-	parser.add_argument('-m', '--MovieName', type=str, help="Target a specific movie inside a movie library based on it's name (only accepted when -l is a movie library); allowed to give argument multiple times", action='append', default=[])
-	parser.add_argument('-s', '--SeriesName', type=str, help="Target a specific series inside a show library based on it's name (only accepted when -l is a show library)")
-	parser.add_argument('-S', '--SeasonNumber', type=int, help="Target a specific season inside the targeted series based on it's number (only accepted when -s is given) (specials is 0)")
-	parser.add_argument('-e', '--EpisodeNumber', type=int, help="Target a specific episode inside the targeted season based on it's number (only accepted when -S is given)")
-	parser.add_argument('-L', '--Limit', type=int, help="Maximum amount of media that the script is allowed to send to the queue")
+    # Setup vars
+    ssn = Session()
+    ssn.headers.update({'Accept': 'application/json'})
+    ssn.params.update({'X-Plex-Token': plex_api_token}) # type: ignore
 
-	args = parser.parse_args()
-	#call function and process result
-	response = hdr_to_sdr_optimizer(ssn=ssn, plex=plex, library_name=args.LibraryName, movie_name=args.MovieName, series_name=args.SeriesName, season_number=args.SeasonNumber, episode_number=args.EpisodeNumber, limit=args.Limit)
-	if not isinstance(response, list):
-		if response == '"season_number" is set but not "series_name"':
-			parser.error('-S/--SeasonNumber given but not -s/--SeriesName given')
+    # Setup arg parsing
+    # autopep8: off
+    parser = ArgumentParser(description="Optimize HDR media when it isn't already available in SDR.")
+    parser.add_argument('-L', '--Limit', type=int, default=-1, help="Maximum amount of media that the script is allowed to send to the queue")
 
-		elif response == '"episode_number" is set but not "season_number" or "series_name"':
-			parser.error('-e/--EpisodeNumber given but -S/--SeasonNumber or -s/--SeriesName not given')
+    ts = parser.add_argument_group(title="Target Selectors")
+    ts.add_argument('-a','--All', action='store_true', help="Target every media item in every library (use with care!)")
+    ts.add_argument('--AllMovie', action='store_true', help="Target all movie libraries")
+    ts.add_argument('--AllShow', action='store_true', help="Target all show libraries")
 
-		else:
-			parser.error(response)
+    ts.add_argument('-l', '--LibraryName', type=str, action='append', default=[], help="Name of target library; allowed to give argument multiple times")
+    ts.add_argument('-m', '--MovieName', type=str, action='append', default=[], help="Target a specific movie inside a movie library based on it's name; allowed to give argument multiple times")
+    ts.add_argument('-s', '--SeriesName', type=str, action='append', default=[], help="Target a specific series inside a show library based on it's name; allowed to give argument multiple times")
+    ts.add_argument('-S', '--SeasonNumber', type=int, action='append', default=[], help="Target a specific season inside the targeted series based on it's number (only accepted when -s is given exactly once) (specials is 0); allowed to give argument multiple times")
+    ts.add_argument('-e', '--EpisodeNumber', type=int, action='append', default=[], help="Target a specific episode inside the targeted season based on it's number (only accepted when -S is given exactly once); allowed to give argument multiple times")
+    # autopep8: on
+
+    args = parser.parse_args()
+
+    plex = PlexServer(base_url, plex_api_token)
+    try:
+        lf = LibraryFilter(
+            all=args.All,
+            all_movie=args.AllMovie,
+            all_show=args.AllShow,
+            libraries=args.LibraryName,
+            movies=args.MovieName,
+            series=args.SeriesName,
+            season_numbers=args.SeasonNumber,
+            episode_numbers=args.EpisodeNumber
+        )
+
+    except ValueError as e:
+        parser.error(e.args[0])
+
+    auto_optimize(
+        ssn=ssn,
+        plex=plex,
+        library_filter=lf,
+        limit=args.Limit
+    )
