e6f06258a4a1477c2ce4a5ca0096cae8e2f8bc2e
CasVT <casvantijn@gmail.com>
2024-09-10 21:58:30 +0200

    Refactored alternate_ordering_playlist.py (Fixes #196)

--- playlist_collection/alternate_ordering_playlist.py
+++ playlist_collection/alternate_ordering_playlist.py
@@ -1,146 +1,268 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	Create a playlist with a series in it but with an alternate order coming from the tvdb
+    Create a playlist with a series in it but with an alternate order
+    coming from the tvdb.
+
 Requirements (python3 -m pip install [requirement]):
-	requests
+    requests
+
 Setup:
-	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+    1. Fill the variables below.
+    2. Run the script in a terminal/shell with the "-h" flag to learn more about the parameters.
+        python3 alternate_ordering_playlist.py -h
+        or
+        python alternate_ordering_playlist.py -h
+
+Examples:
+    --SeriesName "Pokémon" --GetOrders
+
+        List the available orders for the series "Pokémon"
+
+    -s "Initial D" -o "Aired Order" -a
+
+        Make a playlist for the series "Initial D", and order it according to
+        the "Aired Order" ordering. Add any episodes that weren't found on tvdb
+        at the end of the playlist.
 """
 
-plex_ip = ''
-plex_port = ''
-plex_api_token = ''
-
 from os import getenv
-import re
+from re import DOTALL, compile
+from typing import TYPE_CHECKING, Any, Dict, List, Union
+
+if TYPE_CHECKING:
+    from requests import Session
+
+# ===== FILL THESE VARIABLES =====
+plex_base_url = ''
+plex_api_token = ''
+# ================================
 
 # Environmental Variables
-plex_ip = getenv('plex_ip', plex_ip)
-plex_port = getenv('plex_port', plex_port)
+plex_base_url = getenv('plex_base_url', plex_base_url)
 plex_api_token = getenv('plex_api_token', plex_api_token)
-base_url = f"http://{plex_ip}:{plex_port}"
+base_url = plex_base_url.rstrip('/')
+tvdb_base_url = 'https://thetvdb.com'
+order_regex = compile(
+    r'href="#seasons-(\w+?)" class="change_seasontype"[^>]+>(.*?)<')
+episode_regex = compile(
+    r'episode-label">[^>]+>\s+<a href="[^\d]+(\d+)">',
+    DOTALL)
 
-def alternate_ordering_playlist(ssn, series_name: str, get_orders: bool=False, order: str=None, add_unknown: bool=False, no_watched: bool=False):
-	result_json = []
 
-	#check for illegal arg parsing
-	if get_orders == False and order == None:
-		return 'Neither "get_orders" or "order" are set'
+def _find_series(
+    ssn: 'Session',
+    series_name: str
+) -> Dict[str, Any]:
+    search_results: List[dict] = ssn.get(
+        f'{base_url}/search',
+        params={
+            'query': series_name,
+            'includeGuids': '1'
+        }
+    ).json()['MediaContainer'].get('Metadata', [])
 
-	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer']['Directory']
-	#loop through the libraries
-	for lib in sections:
-		if lib['type'] != 'show': continue
+    for search_result in search_results:
+        if (
+            search_result['title'] == series_name
+            and search_result['type'] == 'show'
+        ):
+            break
+    else:
+        raise ValueError("Show not found")
 
-		lib_output = ssn.get(f'{base_url}/library/sections/{lib["key"]}/all', params={'includeGuids': '1'}).json()['MediaContainer']['Metadata']
-		for show in lib_output:
-			if show['title'] == series_name:
-				#show found
-				show_guid = [g['id'] for g in show['Guid'] if g['id'].startswith('tvdb://')]
-				if not show_guid:
-					#tvdb id not found
-					return 'The tvdb id of the series was not found'
-				show_guid = show_guid[0].split('/')[-1]
+    return search_result
 
-				show_info = ssn.get(f'https://thetvdb.com/dereferrer/series/{show_guid}', params={}, headers={}).text
-				seasons = re.search(r'<h2>Seasons</h2>\s+?<ul(.|\s)+?</ul', show_info).group(0)
-				orders = re.findall(r'(?<=>)(?:\w| )+?(?=</a>)', seasons)
-				links = [b.split('"')[-1] for b in re.findall(r'<ul class="list-group list-group-condensed mb-1">.*?<a href=".*?(?=">)', show_info, re.DOTALL)]
-				for index, link in enumerate(links):
-					if link.startswith('/'):
-						links[index] = f'https://thetvdb.com{link}'
 
-				if get_orders == True:
-					#user requests orders
-					return orders
-				if not order in orders:
-					#order user gave is not found
-					return 'Order not found'
+def _get_tvdb_id(data: Dict[str, List[Dict[str, str]]]) -> Union[str, None]:
+    return next(
+        iter((
+            g['id'].split('/')[-1]
+            for g in data.get('Guid', [])
+            if g['id'].startswith('tvdb://')
+        )),
+        None
+    )
 
-				for index, order_entry in enumerate(orders):
-					if order_entry == order:
-						link = links[index]
-						break
-				else:
-					return 'Order not found'
 
-				#get all episodes of series in the order specified and note tvdb ids in order
-				show_content = ssn.get(link, params={}, headers={}).text
-				tvdb_ids = [x.split('/')[-1] for x in re.findall(r'(?<=<a href=")/series/\S+?/episodes/\d+', show_content)]
+def _gather_user_tokens(
+    ssn: 'Session',
+    users: List[str] = ['@me']) -> List[str]:
+    """Get api tokens based on user list.
 
-				#create a map: tvdb id -> plex rating key
-				id_map = {}
-				show_content = ssn.get(f'{base_url}/library/metadata/{show["ratingKey"]}/allLeaves', params={'includeGuids': '1'}).json()['MediaContainer']['Metadata']
-				for episode in show_content:
-					if no_watched == True and 'viewCount' in episode:
-						continue
-					if not 'Guid' in episode: episode['Guid'] = []
-					for id in episode['Guid']:
-						if id['id'].startswith('tvdb://'):
-							id_map[id['id'].split('/')[-1]] = episode['ratingKey']
-							break
-					else:
-						if '_none' in id_map:
-							id_map['_none'].append(episode['ratingKey'])
-						else:
-							id_map['_none'] = [episode['ratingKey']]
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        users (List[str], optional): The list of users to get the tokens of.
+            Defaults to ['@me'].
 
-				#create list of episodes ratingkeys in order of tvdb order
-				for tvdb_id in tvdb_ids:
-					if tvdb_id in id_map:
-						result_json.append(id_map[tvdb_id])
-				if add_unknown == True and '_none' in id_map:
-					result_json += id_map['_none']
+    Returns:
+        List[str]: The list of tokens.
+    """
+    machine_id = ssn.get(
+        f'{base_url}/'
+    ).json()['MediaContainer']['machineIdentifier']
+    shared_users = ssn.get(
+        f'http://plex.tv/api/servers/{machine_id}/shared_servers'
+    ).text
+    user_data = dict(map(
+        lambda r: r.split('"')[0:7:6],
+        shared_users.split('username="')[1:]
+    ))
+    user_data['@me'] = plex_api_token
+    if '@all' not in users:
+        return [v for k, v in user_data.items() if k in users]
+    else:
+        return list(user_data.values())
 
-				#if playlist with this name already exists, remove it first
-				playlists = ssn.get(f'{base_url}/playlists').json()['MediaContainer']
-				if 'Metadata' in playlists:
-					for playlist in playlists['Metadata']:
-						if playlist['title'] == f'{show["title"]} - {order}':
-							ssn.delete(f'{base_url}/playlists/{playlist["ratingKey"]}')
 
-				#create playlist
-				machine_id = ssn.get(f'{base_url}/').json()['MediaContainer']['machineIdentifier']
-				ssn.post(f'{base_url}/playlists', params={'type': 'video', 'title': f'{show["title"]} - {order}', 'smart': '0', 'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join(result_json)}'})
+def alternate_ordering_playlist(
+    ssn: 'Session',
+    series_name: str,
+    get_orders: bool = False,
+    order: Union[str, None] = None,
+    add_unknown: bool = False,
+    no_watched: bool = False,
+    users: List[str] = ['@me']
+) -> Union[List[int], List[str]]:
+    result_json: List[int] = []
 
-				break
-		else:
-			continue
-		break
-	else:
-		return 'Series not found'
+    # Check for illegal arg parsing
+    if not get_orders and order is None:
+        raise ValueError("Either request the orders or make a playlist")
 
-	return result_json
+    series = _find_series(ssn, series_name)
+
+    show_guid = _get_tvdb_id(series)
+
+    if not show_guid:
+        raise ValueError("Show is not matched to the TVDB")
+
+    tvdb_info = ssn.get(
+        f'{tvdb_base_url}/dereferrer/series/{show_guid}'
+    )
+    orders: Dict[str, str] = dict((
+        reversed(o)
+        for o in order_regex.findall(tvdb_info.text)
+    )) # type: ignore
+
+    if get_orders:
+        return list(orders.keys())
+
+    if order not in orders:
+        raise ValueError("Order not found")
+
+    order_link = f"{tvdb_base_url}{tvdb_info.url.split(tvdb_base_url)[1]}/allseasons/{orders[order]}"
+    order_content = ssn.get(order_link).text
+    tvdb_ids: List[str] = episode_regex.findall(order_content)
+
+    # tvdb id -> plex rating key of episode
+    id_map: Dict[str, int] = {}
+    no_id: List[int] = []
+
+    series_content = ssn.get(
+        f'{base_url}/library/metadata/{series["ratingKey"]}/allLeaves',
+        params={'includeGuids': '1'}
+    ).json()['MediaContainer'].get('Metadata', [])
+
+    for episode in series_content:
+        if no_watched and 'viewCount' in episode:
+            continue
+        guid = _get_tvdb_id(episode)
+        if guid:
+            id_map[guid] = int(episode['ratingKey'])
+        elif add_unknown:
+            no_id.append(int(episode['ratingKey']))
+
+    for tvdb_id in tvdb_ids:
+        if tvdb_id in id_map:
+            result_json.append(id_map[tvdb_id])
+
+    if add_unknown:
+        result_json += no_id
+
+    if not result_json:
+        return result_json
+
+    user_tokens = _gather_user_tokens(ssn, users)
+
+    machine_id = ssn.get(
+        f'{base_url}/'
+    ).json()['MediaContainer']['machineIdentifier']
+
+    playlist_name = f"{series['title']} - {order}"
+
+    for token in user_tokens:
+        # If playlist with this name already exists, remove it first
+        playlists = ssn.get(
+            f'{base_url}/playlists',
+            params={'X-Plex-Token': token}
+        ).json()['MediaContainer'].get('Metadata', [])
+
+        for playlist in playlists:
+            if playlist['title'] == playlist_name:
+                ssn.delete(
+                    f'{base_url}/playlists/{playlist["ratingKey"]}',
+                    params={'X-Plex-Token': token}
+                )
+
+        # Create playlist
+        ssn.post(
+            f'{base_url}/playlists',
+            params={
+                'type': 'video',
+                'title': playlist_name,
+                'smart': '0',
+                'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join((str(e) for e in result_json))}',
+                'X-Plex-Token': token})
+
+    return result_json
+
 
 if __name__ == '__main__':
-	import requests, argparse
+    from argparse import ArgumentParser
 
-	#setup vars
-	ssn = requests.Session()
-	ssn.headers.update({'Accept': 'application/json'})
-	ssn.params.update({'X-Plex-Token': plex_api_token})
+    from requests import Session
 
-	#setup arg parsing
-	parser = argparse.ArgumentParser(description="Create a playlist with a series in it but with an alternate order coming from the tvdb")
-	parser.add_argument('-s', '--SeriesName', type=str, help="Target a specific series inside a show library based on it's name", required=True)
-	parser.add_argument('-g', '--GetOrders', help="Get the tvdb orders available for the series", action='store_true')
-	parser.add_argument('-o', '--Order', type=str, help="Name of tvdb order that should be applied")
-	parser.add_argument('-u', '--AddUnknown', help="Add all episodes of the show that weren't found in the tvdb to the end of the playlist", action='store_true')
-	parser.add_argument('-w', '--NoWatched', help="Don't add episodes that are marked as watched", action='store_true')
+    # Setup vars
+    ssn = Session()
+    ssn.headers.update({'Accept': 'application/json'})
+    ssn.params.update({'X-Plex-Token': plex_api_token}) # type: ignore
 
-	args = parser.parse_args()
-	#call function and process result
-	response = alternate_ordering_playlist(ssn=ssn, series_name=args.SeriesName, get_orders=args.GetOrders, order=args.Order, add_unknown=args.AddUnknown, no_watched=args.NoWatched)
-	if isinstance(response, list):
-		if args.GetOrders == True:
-			#the orders of the series was requested so print them out
-			for order in response:
-				print(order)
-	else:
-		if response == 'Neither "get_orders" or "order" are set':
-			parser.error('Neither -g/--GetOrders or -o/--Order was given')
-		else:
-			parser.error(response)
+    # Setup arg parsing
+    # autopep8: off
+    parser = ArgumentParser(description="Create a playlist with a series in it but with an alternate order coming from the tvdb.")
+    parser.add_argument('-s', '--SeriesName', type=str, required=True, help="The series to create the playlist for, based on it's name")
+
+    parser.add_argument('-g', '--GetOrders', action='store_true', help="List the tvdb orders available for the series and exit")
+
+    parser.add_argument('-o', '--Order', type=str, help="Name of tvdb order that should be applied")
+    parser.add_argument('-a', '--AddUnknown', action='store_true', help="Add all episodes of the show that weren't found in the tvdb to the end of the playlist")
+    parser.add_argument('-w', '--NoWatched', action='store_true', help="Don't add episodes that are marked as watched")
+
+    parser.add_argument('-u', '--User', type=str, action='append', default=['@me'], help="Select the user(s) to apply this script to; Give username, '@me' for yourself or '@all' for everyone; allowed to give argument multiple times")
+    # autopep8: on
+
+    args = parser.parse_args()
+
+    try:
+        result = alternate_ordering_playlist(
+            ssn=ssn,
+            series_name=args.SeriesName,
+            get_orders=args.GetOrders,
+            order=args.Order,
+            add_unknown=args.AddUnknown,
+            no_watched=args.NoWatched,
+            users=args.User
+        )
+
+    except ValueError as e:
+        parser.error(e.args[0])
+
+    if not result:
+        print("No orders found or no playlist to make")
+
+    elif isinstance(result[0], str):
+        for order in result:
+            print(order)
