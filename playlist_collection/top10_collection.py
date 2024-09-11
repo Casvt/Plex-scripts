983ea275d4186a995235b71941225fb6036469fb
CasVT <casvantijn@gmail.com>
2024-09-11 10:51:58 +0200

    Refactored top10_collection.py

--- playlist_collection/top10_collection.py
+++ playlist_collection/top10_collection.py
@@ -1,83 +1,173 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	Make a collection with the top 10 most popular movies according to tautulli
+    Make a collection with the top 10 most popular movies.
+
 Requirements (python3 -m pip install [requirement]):
-	requests
+    requests
+
 Setup:
-	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
-	Run this script at an interval. Decide for yourself what the interval is (e.g. every 20m or every 12h)
+    1. Fill the variables below.
+    2. Run the script in a terminal/shell with the "-h" flag to learn more about the parameters.
+        python3 top10_collection.py -h
+        or
+        python top10_collection.py -h
+    3. Run this script at an interval to keep the collection updated.
+        Decide for yourself what the interval is (e.g. every 24h or once every week).
+
+Examples:
+    --LibraryName "Films"
+
+        Create a collection of the top 10 most watched movies in the "Films"
+        library and call the collection "Top 10 Movies".
+
+    -l "Movies" -t "Most Watched Movies"
+
+        Create a collection of the top 10 most watched movies in the "Movies"
+        library and call the collection "Most Watched Movies".
 """
 
-plex_ip = ''
-plex_port = ''
-plex_api_token = ''
-
-tautulli_ip = ''
-tautulli_port = ''
-tautulli_api_token = ''
-
 from os import getenv
-import requests
+from typing import TYPE_CHECKING, List
+
+if TYPE_CHECKING:
+    from requests import Session
+
+# ===== FILL THESE VARIABLES =====
+plex_base_url = ''
+plex_api_token = ''
+# ================================
 
 # Environmental Variables
-plex_ip = getenv('plex_ip', plex_ip)
-plex_port = getenv('plex_port', plex_port)
+plex_base_url = getenv('plex_base_url', plex_base_url)
 plex_api_token = getenv('plex_api_token', plex_api_token)
-plex_base_url = f"http://{plex_ip}:{plex_port}"
-tautulli_ip = getenv('tautulli_ip', tautulli_ip)
-tautulli_port = getenv('tautulli_port', tautulli_port)
-tautulli_api_token = getenv('tautulli_api_token', tautulli_api_token)
-tautulli_base_url = f"http://{tautulli_ip}:{tautulli_port}/api/v2"
+base_url = plex_base_url.rstrip('/')
 
-def top10_collection(ssn, library_name: str, collection_title: str):
-	result_json = []
 
-	sections = ssn.get(f'{plex_base_url}/library/sections').json()['MediaContainer']['Directory']
-	#loop through the libraries
-	for lib in sections:
-		if lib['title'] == library_name:
-			#library found
-			lib_collections = ssn.get(f'{plex_base_url}/library/sections/{lib["key"]}/collections').json()['MediaContainer']
-			if 'Metadata' in lib_collections:
-				#delete old version of collection if present
-				for collection in lib_collections['Metadata']:
-					if collection['title'] == collection_title:
-						collection_id = collection['ratingKey']
-						ssn.delete(f'{plex_base_url}/library/collections/{collection_id}')
-						break
+def _get_library_id(
+    ssn: 'Session',
+    library_name: str
+) -> int:
+    """Get library ID based on it's name.
 
-			#get new list of top movies
-			movies = requests.get(f'{tautulli_base_url}', params={'apikey': tautulli_api_token, 'cmd': 'get_home_stats', 'stat_id': 'top_movies'}).json()['response']['data']['rows']
-			movie_ratingkeys = [str(movie['rating_key']) for movie in movies]
-			result_json = movie_ratingkeys
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        library_name (str): The name of the library to get the ID of.
 
-			#create new collection
-			machine_id = ssn.get(f'{plex_base_url}/').json()['MediaContainer']['machineIdentifier']
-			ssn.post(f'{plex_base_url}/library/collections', params={'type': 1, 'title': collection_title, 'smart': 0, 'sectionId': lib['key'], 'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join(movie_ratingkeys)}'})
-			break
-	else:
-		return 'Library not found'
+    Raises:
+        ValueError: Library not found.
 
-	return result_json
+    Returns:
+        int: The ID of the library.
+    """
+    sections: List[dict] = ssn.get(
+        f'{base_url}/library/sections'
+    ).json()['MediaContainer'].get('Directory', [])
+
+    for lib in sections:
+        if lib['title'] == library_name:
+            return int(lib['key'])
+
+    raise ValueError("Library not found")
+
+
+def _get_top10(
+    ssn: 'Session',
+    library_id: int
+) -> List[int]:
+    """Get the top 10 most watched movies of a library.
+
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        library_id (int): The ID of the library to get the list for.
+
+    Returns:
+        List[int]: The media rating keys of the top 10 most watched movies, in
+        order of watch count.
+    """
+    movies = ssn.get(
+        f'{base_url}/library/sections/{library_id}/all',
+        params={
+            'sort': 'viewCount:desc',
+            'X-Plex-Container-Start': '0',
+            'X-Plex-Container-Size': '10'
+        }
+    ).json()['MediaContainer'].get('Metadata', [])
+
+    return [int(m['ratingKey']) for m in movies]
+
+
+def top10_collection(
+    ssn: 'Session',
+    library_name: str,
+    collection_title: str = "Top 10 Movies"
+) -> List[int]:
+    """Make a collection with the top 10 most popular movies.
+
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        library_name (str): The name of the target library.
+        collection_title (str, optional): The title of the collection.
+            Defaults to "Top 10 Movies".
+
+    Returns:
+        List[int]: List of media rating keys that are in the collection.
+    """
+    lib_id = _get_library_id(ssn, library_name)
+    top10 = _get_top10(ssn, lib_id)
+
+    # If collection with this name already exists, remove it first
+    lib_collections: List[dict] = ssn.get(
+        f'{base_url}/library/sections/{lib_id}/collections'
+    ).json()['MediaContainer'].get('Metadata', [])
+    for collection in lib_collections:
+        if collection['title'] == collection_title:
+            ssn.delete(
+                f'{base_url}/library/collections/{collection["ratingKey"]}'
+            )
+            break
+
+    # Create collection
+    machine_id = ssn.get(
+        f'{base_url}/'
+    ).json()['MediaContainer']['machineIdentifier']
+    ssn.post(
+        f'{base_url}/library/collections',
+        params={
+            'type': '1',
+            'title': collection_title,
+            'smart': '0',
+            'sectionId': lib_id,
+            'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join((str(e) for e in top10))}'
+        }
+    )
+
+    return top10
+
 
 if __name__ == '__main__':
-	import argparse
+    from argparse import ArgumentParser
 
-	#setup vars
-	ssn = requests.Session()
-	ssn.headers.update({'Accept': 'application/json'})
-	ssn.params.update({'X-Plex-Token': plex_api_token})
+    from requests import Session
 
-	#setup arg parsing
-	parser = argparse.ArgumentParser(description='Make a collection with the top 10 most popular movies according to tautulli')
-	parser.add_argument('-l','--LibraryName', type=str, help='Name of target library', required=True)
-	parser.add_argument('-t','--CollectionTitle', type=str, help='The title of the collection', default='Top 10 movies')
+    # Setup vars
+    ssn = Session()
+    ssn.headers.update({'Accept': 'application/json'})
+    ssn.params.update({'X-Plex-Token': plex_api_token}) # type: ignore
 
-	args = parser.parse_args()
-	#call function and process result
-	response = top10_collection(ssn=ssn, library_name=args.LibraryName, collection_title=args.CollectionTitle)
-	if not isinstance(response, list):
-		parser.error(response)
+    # Setup arg parsing
+    # autopep8: off
+    parser = ArgumentParser(description="Make a collection with the top 10 most popular movies of a library.")
+    parser.add_argument('-l', '--LibraryName', type=str, required=True, help="The name of the target library")
+    parser.add_argument('-t', '--CollectionTitle', type=str, default="Top 10 Movies", help="The title of the collection")
+    # autopep8: on
+
+    args = parser.parse_args()
+
+    try:
+        top10_collection(ssn, args.LibraryName, args.CollectionTitle)
+
+    except ValueError as e:
+        parser.error(e.args[0])
