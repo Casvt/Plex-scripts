439107e0ac89176704d30b3805a191a2c47505d0
CasVT <casvantijn@gmail.com>
2024-09-13 11:19:21 +0200

    Refactored push_playlist.py

--- playlist_collection/push_playlist.py
+++ playlist_collection/push_playlist.py
@@ -1,115 +1,220 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	Sync a playlist from one user to the other
-	E.g. a playlist of user1 can be "send" to user2
+    Sync a playlist from one user to one or more other users.
+
 Requirements (python3 -m pip install [requirement]):
-	requests
+    requests
+
 Setup:
-	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+    1. Fill the variables below.
+    2. Run the script in a terminal/shell with the "-h" flag to learn more about the parameters.
+        python3 push_playlist.py -h
+        or
+        python push_playlist.py -h
+
+Examples:
+    --SourceUser "User1" --PlaylistName "Favourite Movies" --TargetUser "User2"
+
+        Push User1's playlist "Favourite Movies" to User2.
+
+    --SourceUser "Batman" --PlaylistName "Villians" --TargetUser "Robin" --TargetUser "Butler"
+
+        Push Batman's playlist "Villains" to Robin and Butler.
+
+    --SourceUser "Admin" --PlaylistName "New Media" --TargetUser "@all"
+
+        Push Admin's playlist "New Media" to all users on the server.
 """
 
-plex_ip = ''
-plex_port = ''
-plex_api_token = ''
-
 from os import getenv
-from re import findall as re_findall
+from typing import TYPE_CHECKING, List
+
+if TYPE_CHECKING:
+    from requests import Session
+
+# ===== FILL THESE VARIABLES =====
+plex_base_url = ''
+plex_api_token = ''
+# ================================
 
 # Environmental Variables
-plex_ip = getenv('plex_ip', plex_ip)
-plex_port = getenv('plex_port', plex_port)
+plex_base_url = getenv('plex_base_url', plex_base_url)
 plex_api_token = getenv('plex_api_token', plex_api_token)
-base_url = f"http://{plex_ip}:{plex_port}"
+base_url = plex_base_url.rstrip('/')
 
-def push_playlist(ssn, source_user: str, target_users: list, playlist_name: str):
-	result_json, source_token, target_tokens = [], '', []
 
-	#check for illegal arg parsing
-	if not target_users:
-		return 'No target users selected'
-	if source_user in target_users:
-		return 'Source user also as target user selected'
-	if source_user == '@all':
-		return 'Source user can not be @all'
+def _gather_user_tokens(
+    ssn: 'Session',
+    users: List[str] = ['@me']) -> List[str]:
+    """Get api tokens based on user list.
 
-	machine_id = ssn.get(f'{base_url}/').json()['MediaContainer']['machineIdentifier']
-	shared_users = ssn.get(f'http://plex.tv/api/servers/{machine_id}/shared_servers', headers={}).text
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        users (List[str], optional): The list of users to get the tokens of.
+            Defaults to ['@me'].
 
-	#add user self to target list if requested
-	if ('@all' in target_users and source_user != '@me') or '@me' in target_users:
-		target_tokens.append(plex_api_token)
+    Returns:
+        List[str]: The list of tokens.
+    """
+    machine_id = ssn.get(
+        f'{base_url}/'
+    ).json()['MediaContainer']['machineIdentifier']
+    shared_users = ssn.get(
+        f'http://plex.tv/api/servers/{machine_id}/shared_servers'
+    ).text
+    user_data = dict(map(
+        lambda r: r.split('"')[0:7:6],
+        shared_users.split('username="')[1:]
+    ))
+    user_data['@me'] = plex_api_token
+    if '@all' not in users:
+        return [v for k, v in user_data.items() if k in users]
+    else:
+        return list(user_data.values())
 
-	#set user self as source if requested
-	if source_user == '@me':
-		source_token = plex_api_token
 
-	#get data about every user (username at beginning and token at end)
-	user_data = re_findall(r'(?<=username=").*?accessToken="\w+?(?=")', shared_users)
-	#loop through shared users; note source user and all target users
-	for user in user_data:
-		username = user.split('"')[0]
-		if not source_token and username == source_user:
-			source_token = user.split('"')[-1]
-			continue
-		if (not '@all' in target_users and not username in target_users) or username == source_user:
-			continue
-		token = user.split('"')[-1]
-		target_tokens.append([username, token])
+def push_playlist(
+    ssn: 'Session',
+    source_user: str,
+    target_users: List[str],
+    playlist_name: str
+) -> List[int]:
+    """Sync a playlist from one user to one or more other users.
 
-	if not source_token:
-		return 'Source user not found'
+    Args:
+        ssn (Session): The plex requests session to fetch with.
 
-	#get source playlist
-	playlists = ssn.get(f'{base_url}/playlists', params={'X-Plex-Token': source_token}).json()['MediaContainer'].get('Metadata', [])
-	for playlist in playlists:
-		if playlist['title'] == playlist_name:
-			#playlist found
-			playlist_ratingkey = playlist['ratingKey']
-			playlist_info = playlist
-			break
-	else:
-		#playlist not found
-		return 'Source playlist not found'
+        source_user (str): Username of the source user; @me to target yourself.
 
-	playlist_content = ssn.get(f'{base_url}/playlists/{playlist_ratingkey}/items', params={'X-Plex-Token': source_token}).json()['MediaContainer'].get('Metadata', [])
-	if not playlist_content: return 'Source playlist is empty'
-	playlist_ratingkeys = [i['ratingKey'] for i in playlist_content]
+        target_users (List[str]): Usernames of the target users;
+            @me to target yourself, @all to target everyone.
 
-	for target_token in target_tokens:
-		print(target_token[0])
-		#delete old playlist if it's there
-		user_playlists = ssn.get(f'{base_url}/playlists', params={'X-Plex-Token': target_token[1]}).json()['MediaContainer'].get('Metadata',[])
-		for user_playlist in user_playlists:
-			if user_playlist['title'] == playlist_name:
-				ssn.delete(f'{base_url}/playlists/{user_playlist["ratingKey"]}', params={'X-Plex-Token': target_token[1]})
+        playlist_name (str): The name of the playlist to push.
 
-		#create playlist for target user
-		new_ratingkey = ssn.post(f'{base_url}/playlists', params={'X-Plex-Token': target_token[1], 'title': playlist_info['title'], 'smart': '0', 'type': playlist_info['playlistType'], 'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join(playlist_ratingkeys)}'}).json()['MediaContainer']['Metadata'][0]['ratingKey']
-		if 'thumb' in playlist_info:
-			#upload poster
-			ssn.post(f'{base_url}/playlists/{new_ratingkey}/posters',  params={'X-Plex-Token': target_token[1], 'url': f'{base_url}{playlist_info["thumb"]}?X-Plex-Token={source_token}'})
+    Raises:
+        ValueError: No target users selected.
+        ValueError: Source user also as target user selected.
+        ValueError: Source user can't be '@all'.
+        ValueError: Source user not found.
+        ValueError: No target users found.
+        ValueError: Source playlist not found.
+        ValueError: Source playlist is empty.
 
-	return result_json
+    Returns:
+        List[int]: List of media rating keys that were in the source playlist.
+    """
+    # Check for illegal arg parsing
+    if not target_users:
+        raise ValueError("No target users selected")
+    if source_user in target_users:
+        raise ValueError("Source user also as target user selected")
+    if source_user == '@all':
+        raise ValueError("Source user can't be '@all'")
+
+    # Gather tokens
+    source_token = _gather_user_tokens(ssn, [source_user])
+    if not source_token:
+        raise ValueError("Source user not found")
+    source_token = source_token[0]
+
+    target_tokens = _gather_user_tokens(ssn, target_users)
+    if not target_tokens:
+        raise ValueError("No target users found")
+
+    # Find source playlist
+    playlists = ssn.get(
+        f'{base_url}/playlists',
+        params={'X-Plex-Token': source_token}
+    ).json()['MediaContainer'].get('Metadata', [])
+
+    for source_playlist in playlists:
+        if source_playlist['title'] == playlist_name:
+            break
+    else:
+        raise ValueError("Source playlist not found")
+
+    # Get content of source playlist
+    playlist_content = ssn.get(
+        f'{base_url}/playlists/{source_playlist["ratingKey"]}/items',
+        params={'X-Plex-Token': source_token}
+    ).json()['MediaContainer'].get('Metadata', [])
+
+    playlist_ratingkeys = [int(i['ratingKey']) for i in playlist_content]
+    if not playlist_ratingkeys:
+        raise ValueError("Source playlist is empty")
+
+    # Create playlists
+    machine_id = ssn.get(
+        f'{base_url}/'
+    ).json()['MediaContainer']['machineIdentifier']
+
+    for target_token in target_tokens:
+        # Delete old playlist if it's there
+        user_playlists = ssn.get(
+            f'{base_url}/playlists',
+            params={'X-Plex-Token': target_token}
+        ).json()['MediaContainer'].get('Metadata', [])
+
+        for user_playlist in user_playlists:
+            if user_playlist['title'] == playlist_name:
+                ssn.delete(
+                    f'{base_url}/playlists/{user_playlist["ratingKey"]}',
+                    params={'X-Plex-Token': target_token}
+                )
+
+        # Create playlist for target user
+        new_ratingkey = ssn.post(
+            f'{base_url}/playlists',
+            params={
+                'X-Plex-Token': target_token,
+                'title': source_playlist['title'],
+                'smart': '0',
+                'type': source_playlist['playlistType'],
+                'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join(str(r) for r in playlist_ratingkeys)}'
+            }
+        ).json()['MediaContainer']['Metadata'][0]['ratingKey']
+
+        if 'thumb' in source_playlist:
+            # Upload poster
+            ssn.post(
+                f'{base_url}/playlists/{new_ratingkey}/posters',
+                params={
+                    'X-Plex-Token': target_token,
+                    'url': f'{base_url}{source_playlist["thumb"]}?X-Plex-Token={source_token}'})
+
+    return playlist_ratingkeys
+
 
 if __name__ == '__main__':
-	import requests, argparse
+    from argparse import ArgumentParser
 
-	#setup vars
-	ssn = requests.Session()
-	ssn.headers.update({'Accept': 'application/json'})
-	ssn.params.update({'X-Plex-Token': plex_api_token})
+    from requests import Session
 
-	#setup arg parsing
-	parser = argparse.ArgumentParser(description='Sync a playlist from one user to the other')
-	parser.add_argument('-s','--SourceUser', type=str, help='Username of the source user; @me to target yourself', required=True)
-	parser.add_argument('-p','--PlaylistName', type=str, help='The name of the playlist to push', required=True)
-	parser.add_argument('-t','--TargetUser', type=str, help='Username of the target user; @me to target yourself; @all to target everyone; give multiple times to target multiple users', action='append', required=True)
+    # Setup vars
+    ssn = Session()
+    ssn.headers.update({'Accept': 'application/json'})
+    ssn.params.update({'X-Plex-Token': plex_api_token}) # type: ignore
 
-	args = parser.parse_args()
-	#call function and process result
-	response = push_playlist(ssn=ssn, source_user=args.SourceUser, target_users=args.TargetUser, playlist_name=args.PlaylistName)
-	if not isinstance(response, list):
-		parser.error(response)
+    # Setup arg parsing
+    # autopep8: off
+    parser = ArgumentParser(description="Sync a playlist from one user to one or more other users.")
+    parser.add_argument('-s', '--SourceUser', type=str, required=True, help="Username of the source user; @me to target yourself")
+    parser.add_argument('-p', '--PlaylistName', type=str, required=True, help="The name of the playlist to push")
+    parser.add_argument('-t', '--TargetUser', type=str, action='append', required=True, help="Select the user(s) to target; Give username, '@me' for yourself or '@all' for everyone; allowed to give argument multiple times")
+    # autopep8: on
+
+    args = parser.parse_args()
+
+    try:
+        push_playlist(
+            ssn,
+            args.SourceUser,
+            args.TargetUser,
+            args.PlaylistName
+        )
+
+    except ValueError as e:
+        parser.error(e.args[0])
