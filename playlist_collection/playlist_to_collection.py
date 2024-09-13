1c11bf875b5d2ff08f26b615be93611827b0468c
CasVT <casvantijn@gmail.com>
2024-09-13 12:06:56 +0200

    Refactored playlist_to_collection.py (Resolves #212)

--- playlist_collection/playlist_to_collection.py
+++ playlist_collection/playlist_to_collection.py
@@ -1,95 +1,179 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	Get the content of a playlist and put it in a collection
+    Get the content of a playlist and put it in a collection.
+
 Requirements (python3 -m pip install [requirement]):
-	requests
+    requests
+
 Setup:
-	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+    1. Fill the variables below.
+    2. Run the script in a terminal/shell with the "-h" flag to learn more about the parameters.
+        python3 playlist_to_collection.py -h
+        or
+        python playlist_to_collection.py -h
+
+Examples:
+    --LibraryName "Films" --PlaylistName "Top Media"
+
+        Put all movies from the playlist "Top Media" in a similarly named collection in the "Films" library.
+
+    -l "Tv-series" -p "Top Media" -r
+
+        Put all shows from the playlist "Top Media" in a similarly named collection in the "Tv-series" library.
+        Remove the playlist afterwards.
 """
 
-plex_ip = ''
-plex_port = ''
-plex_api_token = ''
-
 from os import getenv
+from typing import TYPE_CHECKING, List
+
+if TYPE_CHECKING:
+    from requests import Session
+
+# ===== FILL THESE VARIABLES =====
+plex_base_url = ''
+plex_api_token = ''
+# ================================
 
 # Environmental Variables
-plex_ip = getenv('plex_ip', plex_ip)
-plex_port = getenv('plex_port', plex_port)
+plex_base_url = getenv('plex_base_url', plex_base_url)
 plex_api_token = getenv('plex_api_token', plex_api_token)
-base_url = f"http://{plex_ip}:{plex_port}"
+base_url = plex_base_url.rstrip('/')
 
-def playlist_to_collection(ssn, library_name: str, playlist_name: str, remove_playlist: bool=False):
-	result_json = []
 
-	#get playlist info
-	playlists = ssn.get(f'{base_url}/playlists').json()['MediaContainer']
-	if not 'Metadata' in playlists: return 'Playlist not found'
-	#loop through every playlist
-	for playlist in playlists['Metadata']:
-		if playlist['title'] == playlist_name:
-			#playlist found
-			playlist_id = playlist['ratingKey']
-			playlist_entries = ssn.get(f'{base_url}/playlists/{playlist["ratingKey"]}/items').json()['MediaContainer']
-			if not 'Metadata' in playlist_entries:
-				return 'Playlist is empty'
-			else:
-				playlist_entries = [entry['ratingKey'] for entry in playlist_entries['Metadata']]
-			break
-	else:
-		return 'Playlist not found'
+TYPE_TO_ID = {
+    'movie': '1',
+    'show': '4',
+    'artist': '10',
+    'photo': '13'
+}
 
-	#get library info
-	sections = ssn.get(f'{base_url}/library/sections').json()['MediaContainer']['Directory']
-	#loop through every library
-	for lib in sections:
-		if lib['title'] == library_name:
-			#library found
-			lib_id = lib['key']
-			if lib['type'] == 'movie': lib_type = '1'
-			elif lib['type'] == 'show': lib_type = '4'
-			elif lib['type'] == 'artist': lib_type = '10'
-			elif lib['type'] == 'photo': lib_type = '13'
-			else: return 'Library type not supported'
-			break
-	else:
-		return 'Library not found'
 
-	#remove already existing collection with same name
-	collections = ssn.get(f'{base_url}/library/sections/{lib_id}/collections').json()['MediaContainer']
-	if 'Metadata' in collections:
-		for collection in collections['Metadata']:
-			if collection['title'] == playlist_name:
-				ssn.delete(f'{base_url}/library/collections/{collection["ratingKey"]}')
-				break
+def playlist_to_collection(
+    ssn: 'Session',
+    library_name: str,
+    playlist_name: str,
+    remove_playlist: bool = False
+) -> List[int]:
+    """Get the content of a playlist and put it in a collection.
 
-	#create new collection
-	machine_id = ssn.get(f'{base_url}/').json()['MediaContainer']['machineIdentifier']
-	ssn.post(f'{base_url}/library/collections', params={'type': lib_type, 'title': playlist_name, 'smart': '0', 'sectionId': lib_id, 'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join(playlist_entries)}'})
-	if remove_playlist == True:
-		ssn.delete(f'{base_url}/playlists/{playlist_id}')
+    Args:
+        ssn (Session): The plex requests session to fetch with.
+        library_name (str): Name of library to put collection in.
+        playlist_name (str): Name of playlist to get entries from.
+        remove_playlist (bool, optional): Remove playlist afterwards.
+            Defaults to False.
 
-	return result_json
+    Raises:
+        ValueError: Playlist not found.
+        ValueError: Library not found.
+        ValueError: Library type not supported.
+        ValueError: Playlist is empty.
+
+    Returns:
+        List[int]: List of media rating keys in playlist.
+    """
+    # Find playlist
+    playlists: List[dict] = ssn.get(
+        f'{base_url}/playlists'
+    ).json()['MediaContainer'].get('Metadata', [])
+    for playlist in playlists:
+        if playlist['title'] == playlist_name:
+            break
+    else:
+        raise ValueError("Playlist not found")
+
+    # Find library
+    sections = ssn.get(
+        f'{base_url}/library/sections'
+    ).json()['MediaContainer'].get('Directory', [])
+    for lib in sections:
+        if lib['title'] == library_name:
+            break
+    else:
+        raise ValueError("Library not found")
+
+    if not lib['type'] in TYPE_TO_ID:
+        raise ValueError("Library type not supported")
+
+    # Get playlist content
+    playlist_entries = ssn.get(
+        f'{base_url}/playlists/{playlist["ratingKey"]}/items'
+    ).json()['MediaContainer'].get('Metadata', [])
+
+    playlist_entries = [int(e['ratingKey']) for e in playlist_entries]
+    if not playlist_entries:
+        raise ValueError("Playlist is empty")
+
+    # Remove already existing collection with same name
+    collections = ssn.get(
+        f'{base_url}/library/sections/{lib["key"]}/collections'
+    ).json()['MediaContainer'].get('Metadata', [])
+    for collection in collections:
+        if collection['title'] == playlist_name:
+            ssn.delete(
+                f'{base_url}/library/collections/{collection["ratingKey"]}'
+            )
+            break
+
+    # Create new collection
+    machine_id = ssn.get(
+        f'{base_url}/'
+    ).json()['MediaContainer']['machineIdentifier']
+
+    new_ratingkey = ssn.post(
+        f'{base_url}/library/collections',
+        params={
+            'type': TYPE_TO_ID[lib['type']],
+            'title': playlist_name,
+            'smart': '0',
+            'sectionId': lib['key'],
+            'uri': f'server://{machine_id}/com.plexapp.plugins.library/library/metadata/{",".join((str(e) for e in playlist_entries))}'
+        }
+    ).json()['MediaContainer']['Metadata'][0]['ratingKey']
+
+    if 'thumb' in playlist:
+        # Upload poster
+        ssn.post(
+            f'{base_url}/library/collections/{new_ratingkey}/posters',
+            params={
+                'url': f'{base_url}{playlist["thumb"]}?X-Plex-Token={plex_api_token}'})
+
+    if remove_playlist:
+        ssn.delete(f'{base_url}/playlists/{playlist["ratingKey"]}')
+
+    return playlist_entries
+
 
 if __name__ == '__main__':
-	import requests, argparse
+    from argparse import ArgumentParser
 
-	#setup vars
-	ssn = requests.Session()
-	ssn.headers.update({'Accept': 'application/json'})
-	ssn.params.update({'X-Plex-Token': plex_api_token})
+    from requests import Session
 
-	#setup arg parsing
-	parser = argparse.ArgumentParser(description='Get the content of a playlist and put it in a collection')
-	parser.add_argument('-l','--LibraryName', type=str, help='Name of target library', required=True)
-	parser.add_argument('-p','--PlaylistName', type=str, help='Name of target playlist', required=True)
-	parser.add_argument('-r','--RemovePlaylist', help='Remove source playlist afterwards', action='store_true')
+    # Setup vars
+    ssn = Session()
+    ssn.headers.update({'Accept': 'application/json'})
+    ssn.params.update({'X-Plex-Token': plex_api_token}) # type: ignore
 
-	args = parser.parse_args()
-	#call function and process result
-	response = playlist_to_collection(ssn=ssn, library_name=args.LibraryName, playlist_name=args.PlaylistName, remove_playlist=args.RemovePlaylist)
-	if not isinstance(response, list):
-		parser.error(response)
+    # Setup arg parsing
+    # autopep8: off
+    parser = ArgumentParser(description="Get the content of a playlist and put it in a collection.")
+    parser.add_argument('-l', '--LibraryName', type=str, required=True, help="Name of target library")
+    parser.add_argument('-p', '--PlaylistName', type=str, required=True, help="Name of target playlist")
+    parser.add_argument('-r', '--RemovePlaylist', action='store_true', help="Remove source playlist afterwards")
+    # autopep8: on
+
+    args = parser.parse_args()
+
+    try:
+        playlist_to_collection(
+            ssn,
+            args.LibraryName,
+            args.PlaylistName,
+            args.RemovePlaylist
+        )
+
+    except ValueError as e:
+        parser.error(e.args[0])
