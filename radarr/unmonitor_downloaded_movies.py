fe0acab18088a87f0dd38212336f8b2a1f49787a
CasVT <casvantijn@gmail.com>
2024-09-14 18:09:13 +0200

    Refactored unmonitor_downloaded_movies.py

--- radarr/unmonitor_downloaded_movies.py
+++ radarr/unmonitor_downloaded_movies.py
@@ -1,48 +1,88 @@
 #!/usr/bin/python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	When a movie is downloaded and imported in radarr, unmonitor that movie
+    When a movie is downloaded and imported in radarr, unmonitor that movie.
+
 Setup:
-	Fill the variables below firstly,
-	then go to the radarr web-ui -> Settings -> Connect -> + -> Custom Script:
-		Name = whatever you want
-		Triggers = 'On Import' and 'On Upgrade'
-		Tags = whatever if needed
-		path = /path/to/unmonitor_downloaded_movies.py
+    1. Fill the variables below.
+    2. Go to the Radarr web-UI -> Settings -> Connect -> + -> Custom Script:
+        Name = whatever you want
+        Triggers = 'On Import' and 'On Upgrade'
+        Tags = whatever if needed
+        path = /path/to/unmonitor_downloaded_movies.py
 """
 
-radarr_ip = ''
-radarr_port = ''
-radarr_api_token = ''
-
+from json import dumps, loads
 from os import getenv
+from urllib.request import Request, urlopen
+
+# ===== FILL THESE VARIABLES =====
+radarr_base_url = ''
+radarr_api_token = ''
+# ================================
 
 # Environmental Variables
-radarr_ip = getenv('radarr_ip', radarr_ip)
-radarr_port = getenv('radarr_port', radarr_port)
+radarr_base_url = getenv('radarr_base_url', radarr_base_url)
 radarr_api_token = getenv('radarr_api_token', radarr_api_token)
+base_url = radarr_base_url.rstrip('/')
 
-def unmonitor_downloaded_movies(ssn, movie_id: str):
-	result = ssn.put(f'http://{radarr_ip}:{radarr_port}/api/v3/movie/editor', json={'movieIds':[movie_id],'monitored': False})
-	return result
 
-#handle testing of the script by radarr
-if getenv('radarr_eventtype') == 'Test':
-	if radarr_ip and radarr_port and radarr_api_token:
-		exit(0)
-	else:
-		print('Error: Not all variables are set')
-		exit(1)
+def _get_api_version() -> str:
+    """Fetch the api version
+
+    Returns:
+        str: The api version. E.g. 'v3'.
+    """
+    with urlopen(f'{base_url}/api?apikey={radarr_api_token}') as resp:
+        api_version: str = loads(resp.read().decode('utf-8'))["current"]
+    return api_version
+
+
+def unmonitor_downloaded_movies(
+    movie_id: str,
+    api_version: str = 'v3'
+) -> bool:
+    """Unmonitor a movie based on it's Radarr ID.
+
+    Args:
+        movie_id (str): The ID of the movie to unmonitor.
+
+        api_version (str, optional): The API version of the Radarr instance.
+            Defaults to 'v3'.
+
+    Returns:
+        bool: Whether or not there was a network error.
+    """
+    data = {
+        'movieIds': [movie_id],
+        'monitored': False
+    }
+    send_data = dumps(data)
+
+    req = Request(
+        url=f"{base_url}/api/{api_version}/movie/editor?apikey={radarr_api_token}",
+        data=send_data.encode("utf-8"),
+        headers={
+            'Content-Type': 'application/json; charset=UTF-8'},
+        method="PUT")
+
+    with urlopen(req) as resp:
+        return resp.getcode() < 400
+
 
 if __name__ == '__main__':
-	import requests
+    # Handle testing of the script by radarr
+    if getenv('radarr_eventtype') == 'Test':
+        if not radarr_base_url or not radarr_api_token:
+            print('Error: Not all variables are set')
+            exit(1)
 
-	#setup vars
-	ssn = requests.Session()
-	ssn.params.update({'apikey': radarr_api_token})
-	movie_id = str(getenv('radarr_movie_id'))
+        _get_api_version()
 
-	#call function
-	unmonitor_downloaded_movies(ssn, movie_id)
+        exit(0)
+
+    movie_id = str(getenv('radarr_movie_id'))
+
+    unmonitor_downloaded_movies(movie_id, _get_api_version())
