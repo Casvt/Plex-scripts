20f2e56a88435d505c576617f6e79128d453cadc
CasVT <casvantijn@gmail.com>
2024-09-19 09:02:41 +0200

    Refactored unmonitor_downloaded_episodes.py

--- sonarr/unmonitor_downloaded_episodes.py
+++ sonarr/unmonitor_downloaded_episodes.py
@@ -1,48 +1,88 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	When an episode is downloaded and imported in sonarr, unmonitor that episode
+    When an episode is downloaded and imported in sonarr, unmonitor that episode.
+
 Setup:
-	Fill the variables below firstly,
-	then go to the sonarr web-ui -> Settings -> Connect -> + -> Custom Script:
-		Name = whatever you want
-		Triggers = 'On Download' and 'On Upgrade'
-		Tags = whatever if needed
-		path = /path/to/unmonitor_downloaded_episodes.py
+    1. Fill the variables below.
+    2. Go to the Sonarr web-UI -> Settings -> Connect -> + -> Custom Script:
+        Name = whatever you want
+        Triggers = 'On Import' and 'On Upgrade'
+        Tags = whatever if needed
+        path = /path/to/unmonitor_downloaded_episodes.py
 """
 
-sonarr_ip = ''
-sonarr_port = ''
-sonarr_api_token = ''
-
+from json import dumps, loads
 from os import getenv
+from urllib.request import Request, urlopen
+
+# ===== FILL THESE VARIABLES =====
+sonarr_base_url = ''
+sonarr_api_token = ''
+# ================================
 
 # Environmental Variables
-sonarr_ip = getenv('sonarr_ip', sonarr_ip)
-sonarr_port = getenv('sonarr_port', sonarr_port)
+sonarr_base_url = getenv('sonarr_base_url', sonarr_base_url)
 sonarr_api_token = getenv('sonarr_api_token', sonarr_api_token)
+base_url = sonarr_base_url.rstrip('/')
 
-def unmonitor_downloaded_episodes(ssn, episode_id: str):
-	result = ssn.put(f'http://{sonarr_ip}:{sonarr_port}/api/v3/episode/monitor', json={'episodeIds':[episode_id],'monitored': False})
-	return result
 
-#handle testing of the script by sonarr
-if getenv('sonarr_eventtype') == 'Test':
-	if sonarr_ip and sonarr_port and sonarr_api_token:
-		exit(0)
-	else:
-		print('Error: Not all variables are set')
-		exit(1)
+def _get_api_version() -> str:
+    """Fetch the api version
+
+    Returns:
+        str: The api version. E.g. 'v3'.
+    """
+    with urlopen(f'{base_url}/api?apikey={sonarr_api_token}') as resp:
+        api_version: str = loads(resp.read().decode('utf-8'))["current"]
+    return api_version
+
+
+def unmonitor_downloaded_episodes(
+    episode_id: str,
+    api_version: str = 'v3'
+) -> bool:
+    """Unmonitor an episode based on it's Sonarr ID.
+
+    Args:
+        episode_id (str): The ID of the episode to unmonitor.
+
+        api_version (str, optional): The API version of the Sonarr instance.
+            Defaults to 'v3'.
+
+    Returns:
+        bool: Whether or not there was a network error.
+    """
+    data = {
+        'episodeIds': [episode_id],
+        'monitored': False
+    }
+    send_data = dumps(data)
+
+    req = Request(
+        url=f"{base_url}/api/{api_version}/episode/monitor?apikey={sonarr_api_token}",
+        data=send_data.encode("utf-8"),
+        headers={
+            'Content-Type': 'application/json; charset=UTF-8'},
+        method="PUT")
+
+    with urlopen(req) as resp:
+        return resp.getcode() < 400
+
 
 if __name__ == '__main__':
-	import requests
+    # Handle testing of the script by sonarr
+    if getenv('sonarr_eventtype') == 'Test':
+        if not sonarr_base_url or not sonarr_api_token:
+            print('Error: Not all variables are set')
+            exit(1)
 
-	#setup vars
-	ssn = requests.Session()
-	ssn.params.update({'apikey': sonarr_api_token})
-	episode_id = str(getenv('sonarr_episodefile_episodeids'))
+        _get_api_version()
 
-	#call function
-	unmonitor_downloaded_episodes(ssn, episode_id)
+        exit(0)
+
+    episode_id = str(getenv('sonarr_episodefile_id'))
+
+    unmonitor_downloaded_episodes(episode_id, _get_api_version())
