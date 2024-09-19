b24b6dd803306ea769c3570e0b6426c6d27650fc
CasVT <casvantijn@gmail.com>
2024-09-19 08:53:00 +0200

    Refactored remove_codec.py

--- sonarr/remove_codec.py
+++ sonarr/remove_codec.py
@@ -1,103 +1,214 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	Delete media files in sonarr/radarr that have the codec given and initiate a new search for them
+    Delete media files in Sonarr/Radarr that have the codec given
+    and initiate a new search for them.
+
 Requirements (python3 -m pip install [requirement]):
-	requests
+    requests
+
 Setup:
-	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+    1. Fill the variables below.
+    2. Run the script in a terminal/shell with the "-h" flag to learn more about the parameters.
+        python3 remove_codec.py -h
+        or
+        python remove_codec.py -h
+
+Examples:
+    --Source radarr --Codec x264
+
+        Find all movies on Radarr that have the x264 codec.
+        Then delete them and initiate a search.
+
+    -s sonarr -c av1
+
+        Find all episodes on Sonarr that have the av1 codec.
+        Then delete them and initiate a search.
 """
 
-#These need to be filled when you want to use the script with sonarr
-sonarr_ip = ''
-sonarr_port = ''
-sonarr_api_token = ''
-
-#These need to be filled when you want to use the script with radarr
-radarr_ip = ''
-radarr_port = ''
-radarr_api_token = ''
-
+from enum import Enum
 from os import getenv
-import requests
+from typing import TYPE_CHECKING, List, Union
+
+if TYPE_CHECKING:
+    from requests import Session
+
+# ===== FILL THESE VARIABLES TO USE SONARR =====
+sonarr_base_url = ''
+sonarr_api_token = ''
+# ===== FILL THESE VARIABLES TO USE RADARR =====
+radarr_base_url = ''
+radarr_api_token = ''
+# ==============================================
 
 # Environmental Variables
-sonarr_ip = getenv('sonarr_ip', sonarr_ip)
-sonarr_port = getenv('sonarr_port', sonarr_port)
+sonarr_base_url = getenv('sonarr_base_url', sonarr_base_url)
 sonarr_api_token = getenv('sonarr_api_token', sonarr_api_token)
-radarr_ip = getenv('radarr_ip', radarr_ip)
-radarr_port = getenv('radarr_port', radarr_port)
+radarr_base_url = getenv('radarr_base_url', radarr_base_url)
 radarr_api_token = getenv('radarr_api_token', radarr_api_token)
+s_base_url = sonarr_base_url.rstrip('/') + '/api/v3'
+r_base_url = radarr_base_url.rstrip('/') + '/api/v3'
 
-def remove_codec(source: str, codec: str):
-	result_json = []
 
-	if source == 'sonarr':
-		if sonarr_ip and sonarr_port and sonarr_api_token:
-			#apply script to sonarr
-			sonarr_base_url = f'http://{sonarr_ip}:{sonarr_port}/api/v3'
-			sonarr_ssn = requests.Session()
-			sonarr_ssn.params.update({'apikey': sonarr_api_token})
-			try:
-				series_list = sonarr_ssn.get(f'{sonarr_base_url}/series').json()
-			except requests.exceptions.ConnectionError:
-				return 'Can\'t connect to Sonarr'
+class SoftwareType(Enum):
+    radarr = 1
+    sonarr = 2
 
-			#loop through all series in sonarr
-			for series in series_list:
-				episode_list = sonarr_ssn.get(f'{sonarr_base_url}/episode', params={'seriesId': series['id']}).json()
-				for episode in episode_list:
-					episode_output = sonarr_ssn.get(f'{sonarr_base_url}/episodeFile/{episode["episodeFileId"]}').json()
-					if episode_output['mediaInfo']['videoCodec'] == codec:
-						#episode matches codec; replace it
-						print(episode_output['path'])
-						#delete media file
-						sonarr_ssn.delete(f'{sonarr_base_url}/episodeFile/{episode_output["id"]}', params={'episodeEntity': 'episodes'})
-						#start search for new file
-						sonarr_ssn.post(f'{sonarr_base_url}/command', json={'name':'EpisodeSearch', 'episodeIds': [episode['id']]})
-						result_json.append(episode['id'])
-		else:
-			return 'Sonarr set as source but variables not set'
 
-	elif source == 'radarr':
-		if radarr_ip and radarr_port and radarr_api_token:
-			#apply script to sonarr
-			radarr_base_url = f'http://{radarr_ip}:{radarr_port}/api/v3'
-			radarr_ssn = requests.Session()
-			radarr_ssn.params.update({'apikey': radarr_api_token})
-			try:
-				movie_list = radarr_ssn.get(f'{radarr_base_url}/movie').json()
-			except requests.exceptions.ConnectionError:
-				return 'Can\'t connect to Radarr'
+def _sonarr(
+    sonarr_ssn: 'Session',
+    codec: str
+) -> List[int]:
 
-			#loop through all movies in radarr
-			for movie in movie_list:
-				if 'movieFile' in movie:
-					if movie['movieFile']['mediaInfo']['videoCodec'] == codec:
-						#movie matches codec; replace it
-						print(movie['movieFile']['path'])
-						#delete media file
-						radarr_ssn.delete(f'{radarr_base_url}/movieFile/{movie["movieFile"]["id"]}')
-						#start search for new file
-						radarr_ssn.post(f'{radarr_base_url}/command', json={'name':'MoviesSearch', 'movieIds':[movie['id']]})
-						result_json.append(movie['id'])
-		else:
-			return 'Radarr set as source but variables not set'
+    result_json: List[int] = []
+    series_list: List[dict] = sonarr_ssn.get(
+        f'{sonarr_base_url}/series'
+    ).json()
 
-	return result_json
+    for series in series_list:
+        episode_list: List[dict] = sonarr_ssn.get(
+            f'{sonarr_base_url}/episode',
+            params={'seriesId': series['id']}
+        ).json()
+
+        for episode in episode_list:
+            episode_output: dict = sonarr_ssn.get(
+                f'{sonarr_base_url}/episodeFile/{episode["episodeFileId"]}'
+            ).json()
+
+            if episode_output['mediaInfo']['videoCodec'] != codec:
+                continue
+
+            # Episode matches codec; replace it
+            print(episode_output['path'])
+            sonarr_ssn.delete(
+                f'{sonarr_base_url}/episodeFile/{episode_output["id"]}',
+                params={'episodeEntity': 'episodes'}
+            )
+            sonarr_ssn.post(
+                f'{sonarr_base_url}/command',
+                json={
+                    'name': 'EpisodeSearch',
+                    'episodeIds': [episode['id']]
+                }
+            )
+            result_json.append(episode['id'])
+
+    return result_json
+
+
+def _radarr(
+    radarr_ssn: 'Session',
+    codec: str
+) -> List[int]:
+
+    result_json: List[int] = []
+    movie_list: List[dict] = radarr_ssn.get(
+        f'{radarr_base_url}/movie'
+    ).json()
+
+    for movie in movie_list:
+        if 'movieFile' not in movie:
+            continue
+        if movie['movieFile']['mediaInfo']['videoCodec'] != codec:
+            continue
+
+        # Movie matches codec; replace it
+        print(movie['movieFile']['path'])
+        radarr_ssn.delete(
+            f'{radarr_base_url}/movieFile/{movie["movieFile"]["id"]}'
+        )
+        radarr_ssn.post(
+            f'{radarr_base_url}/command',
+            json={
+                'name': 'MoviesSearch',
+                'movieIds': [movie['id']]
+            }
+        )
+        result_json.append(movie['id'])
+
+    return result_json
+
+
+def remove_codec(
+    radarr_ssn: Union['Session', None],
+    sonarr_ssn: Union['Session', None],
+    source: SoftwareType,
+    codec: str
+) -> List[int]:
+    """Delete media files in Sonarr/Radarr that have the codec given
+    and initiate a new search for them.
+
+    Args:
+        radarr_ssn (Union[Session, None]): The radarr requests
+        session to fetch with, if radarr is the source to work on.
+        sonarr_ssn (Union[Session, None]): The sonarr requests
+        session to fetch with, if sonarr is the source to work on.
+        source (SoftwareType): The type to work on.
+        codec (str): The codec to search for.
+
+    Returns:
+        List[int]: List of media ID's that were processed.
+    """
+    result_json: List[int] = []
+
+    if (
+        source == SoftwareType.sonarr
+        and sonarr_ssn is not None
+    ):
+        result_json = _sonarr(sonarr_ssn, codec)
+
+    elif (
+        source == SoftwareType.radarr
+        and radarr_ssn is not None
+    ):
+        result_json = _radarr(radarr_ssn, codec)
+
+    return result_json
+
 
 if __name__ == '__main__':
-	import argparse
+    from argparse import ArgumentParser
 
-	#setup arg parsing
-	parser = argparse.ArgumentParser(description="Delete media files in sonarr/radarr that have the codec given and initiate a new search for them")
-	parser.add_argument('-s', '--Source', type=str, choices=['sonarr','radarr'], help="Select the source which media files should be checked", required=True)
-	parser.add_argument('-c', '--Codec', type=str, help="Media files with this codec will be removed and a new search will be initiated", required=True)
+    from requests import Session
 
-	args = parser.parse_args()
-	#call function and process result
-	response = remove_codec(source=args.Source, codec=args.Codec)
-	if not isinstance(response, list):
-		parser.error(response)
+    # Setup arg parsing
+    # autopep8: off
+    parser = ArgumentParser(description="Delete media files in Sonarr/Radarr that have the codec given and initiate a new search for them.")
+    parser.add_argument('-s', '--Source', type=str, choices=SoftwareType._member_names_, required=True, help="Select the source which media files should be checked")
+    parser.add_argument('-c', '--Codec', type=str, required=True, help="Media files with this codec will be removed and a new search will be initiated")
+    # autopep8: on
+
+    args = parser.parse_args()
+
+    try:
+        source = SoftwareType[args.Source]
+        radarr_ssn, sonarr_ssn = None, None
+
+        if source == SoftwareType.radarr:
+            if not (radarr_base_url and radarr_api_token):
+                raise ValueError("Radarr not set up")
+
+            radarr_ssn = Session()
+            radarr_ssn.params.update( # type: ignore
+                {'apikey': radarr_api_token}
+            )
+
+        elif source == SoftwareType.sonarr:
+            if not (sonarr_base_url and sonarr_api_token):
+                raise ValueError("Sonarr not set up")
+
+            sonarr_ssn = Session()
+            sonarr_ssn.params.update( # type: ignore
+                {'apikey': radarr_api_token}
+            )
+
+        remove_codec(
+            radarr_ssn, sonarr_ssn,
+            source, args.Codec
+        )
+
+    except ValueError as e:
+        parser.error(e.args[0])
