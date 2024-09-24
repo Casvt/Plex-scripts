a9c94385242c86147c63874f76ccdcdf9eb4d5fb
CasVT <casvantijn@gmail.com>
2024-09-24 22:31:59 +0200

    Refactored sonarr_refresh_tba.py (Fixed #116)

--- sonarr/sonarr_refresh_tba.py
+++ sonarr/sonarr_refresh_tba.py
@@ -1,90 +1,219 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 The use case of this script is the following:
-	Refresh sonarr- or plex series for all TBA episodes
+    Refresh Sonarr- or Plex series for all TBA episodes.
+
 Requirements (python3 -m pip install [requirement]):
-	requests
+    requests
+
 Setup:
-	Fill the variables below firstly, then run the script with -h to see the arguments that you need to give.
+    1. Fill the variables below.
+    2. Run the script in a terminal/shell with the "-h" flag to learn more about the parameters.
+        python3 sonarr_refresh_tba.py -h
+        or
+        python sonarr_refresh_tba.py -h
+
+Examples:
+    --Target plex --Target sonarr
+
+        Trigger a refresh for all TBA/TBD episodes found on plex and sonarr.
 """
 
-sonarr_ip = ''
-sonarr_port = ''
-sonarr_api_token = ''
-
-plex_ip = ''
-plex_port = ''
-plex_api_token = ''
-
+from enum import Enum
 from os import getenv
+from typing import TYPE_CHECKING, List, Tuple, Union
+
+if TYPE_CHECKING:
+    from requests import Session
+
+# ===== FILL THESE VARIABLES TO USE PELX =======
+plex_base_url = ''
+plex_api_token = ''
+# ===== FILL THESE VARIABLES TO USE SONARR =====
+sonarr_base_url = ''
+sonarr_api_token = ''
+# ==============================================
 
 # Environmental Variables
-sonarr_ip = getenv('sonarr_ip', sonarr_ip)
-sonarr_port = getenv('sonarr_port', sonarr_port)
-sonarr_api_token = getenv('sonarr_api_token', sonarr_api_token)
-sonarr_base_url = f"http://{sonarr_ip}:{sonarr_port}/api/v3"
-plex_ip = getenv('plex_ip', plex_ip)
-plex_port = getenv('plex_port', plex_port)
+plex_base_url = getenv('plex_base_url', plex_base_url)
 plex_api_token = getenv('plex_api_token', plex_api_token)
-plex_base_url = f'http://{plex_ip}:{plex_port}'
+sonarr_base_url = getenv('sonarr_base_url', sonarr_base_url)
+sonarr_api_token = getenv('sonarr_api_token', sonarr_api_token)
+p_base_url = plex_base_url.rstrip('/')
+s_base_url = sonarr_base_url.rstrip('/') + '/api/v3'
 
-def sonarr_refresh_tba(sonarr_ssn, plex_ssn, target: list):
-	result_json = []
 
-	if 'sonarr' in target:
-		#find episodes in sonarr that are TBA
-		series_list = sonarr_ssn.get(f'{sonarr_base_url}/series').json()
-		for series in series_list:
-			series_output = sonarr_ssn.get(f'{sonarr_base_url}/episode', params={'seriesId': series['id']}).json()
-			for episode in series_output:
-				if episode['title'].lower() in ('tba','tbd'):
-					#episode found that is TBA
-					print(f'{series["title"]}')
+class SoftwareType(Enum):
+    plex = 1
+    sonarr = 2
 
-					#refresh episode
-					sonarr_ssn.post(f'{sonarr_base_url}/command', json={'name': 'RefreshSeries','seriesId': series['id']})
 
-					result_json.append(episode['id'])
-					break
+def _plex(
+    plex_ssn: 'Session'
+) -> List[int]:
+    result_json: List[int] = []
 
-	if 'plex' in target:
-		#find episodes in plex that are TBA
-		sections = plex_ssn.get(f'{plex_base_url}/library/sections').json()['MediaContainer'].get('Directory',[])
-		for lib in sections:
-			if lib['type'] == 'show':
-				#found show library; check every episode
-				lib_content = plex_ssn.get(f'{plex_base_url}/library/sections/{lib["key"]}/all', params={'type': '4'}).json()['MediaContainer'].get('Metadata',[])
-				for episode in lib_content:
-					if episode['title'].lower() in ('tba','tbd'):
-						#episode found that is TBA
-						print(f'{episode["grandparentTitle"]} - S{episode["parentIndex"]}E{episode["index"]}')
+    sections = plex_ssn.get(
+        f'{p_base_url}/library/sections'
+    ).json()['MediaContainer'].get('Directory', [])
 
-						#refresh metadata of episode
-						plex_ssn.put(f'{plex_base_url}/library/metadata/{episode["ratingKey"]}/refresh')
+    for lib in sections:
+        if lib['type'] != 'show':
+            continue
 
-						result_json.append(episode['ratingKey'])
+        # Found show library; check every episode
+        lib_content = plex_ssn.get(
+            f'{p_base_url}/library/sections/{lib["key"]}/all',
+            params={'type': '4'}
+        ).json()['MediaContainer'].get('Metadata', [])
 
-	return result_json
+        for episode in lib_content:
+            if (
+                episode['title']
+                .lower()
+                .strip()
+                .rstrip('.')
+                not in ('tba', 'tbd')
+            ):
+                continue
+
+            # Episode found that is TBA; refresh it
+            print(
+                f'{episode["grandparentTitle"]} - S{episode["parentIndex"]}E{episode["index"]}')
+
+            plex_ssn.put(
+                f'{p_base_url}/library/metadata/{episode["ratingKey"]}/refresh'
+            )
+
+            result_json.append(int(episode['ratingKey']))
+
+    return []
+
+
+def _sonarr(
+    sonarr_ssn: 'Session'
+) -> List[int]:
+    result_json: List[int] = []
+
+    series_list = sonarr_ssn.get(
+        f'{s_base_url}/series'
+    ).json()
+
+    for series in series_list:
+
+        series_output = sonarr_ssn.get(
+            f'{s_base_url}/episode',
+            params={'seriesId': series['id']}
+        ).json()
+
+        for episode in series_output:
+            if (
+                episode['title']
+                .lower()
+                .strip()
+                .rstrip('.')
+                not in ('tba', 'tbd')
+            ):
+                continue
+
+            # Episode found that is TBA; refresh series
+            print(f'{series["title"]}')
+
+            sonarr_ssn.post(
+                f'{s_base_url}/command',
+                json={
+                    'name': 'RefreshSeries',
+                    'seriesId': series['id']
+                }
+            )
+
+            result_json.append(series['id'])
+            break
+
+    return result_json
+
+
+def sonarr_refresh_tba(
+    plex_ssn: Union['Session', None],
+    sonarr_ssn: Union['Session', None],
+    sources: List[SoftwareType]
+) -> Tuple[List[int], List[int]]:
+    """Refresh Sonarr- or Plex series for all TBA episodes.
+
+    Args:
+        plex_ssn (Union[Session, None]): The plex requests session
+        to fetch with, if plex is one of the sources to work on.
+
+        sonarr_ssn (Union[Session, None]): The sonarr requests session
+        to fetch with, if sonarr is one of the sources to work on.
+
+        sources (List[SoftwareType]): The types to work on.
+
+    Returns:
+        Tuple[List[int], List[int]]: First list is plex rating keys that were
+        updated. Second lists is sonarr series ID's that were updated.
+    """
+    result_json: Tuple[List[int], List[int]] = ([], [])
+
+    if (
+        SoftwareType.plex in sources
+        and plex_ssn is not None
+    ):
+        result_json[0].extend(_plex(plex_ssn))
+
+    if (
+        SoftwareType.sonarr in sources
+        and sonarr_ssn is not None
+    ):
+        result_json[1].extend(_sonarr(sonarr_ssn))
+
+    return result_json
+
 
 if __name__ == '__main__':
-	from requests import Session
-	from argparse import ArgumentParser
+    from argparse import ArgumentParser
 
-	#setup vars
-	sonarr_ssn = Session()
-	sonarr_ssn.params.update({'apikey': sonarr_api_token})
-	plex_ssn = Session()
-	plex_ssn.headers.update({'Accept': 'application/json'})
-	plex_ssn.params.update({'X-Plex-Token': plex_api_token})
+    from requests import Session
 
-	#setup arg parsing
-	parser = ArgumentParser(description="Refresh sonarr- or plex series for all TBA episodes")
-	parser.add_argument('-t','--Target', action='append', choices=['plex','sonarr'], required=True)
+    # Setup arg parsing
+    # autopep8: off
+    parser = ArgumentParser(description="Refresh Sonarr- or Plex series for all TBA episodes.")
+    parser.add_argument('-t', '--Target', action='append', required=True, choices=SoftwareType._member_names_)
+    # autopep8: on
 
-	args = parser.parse_args()
-	#call function and process result
-	response = sonarr_refresh_tba(sonarr_ssn=sonarr_ssn, plex_ssn=plex_ssn, target=args.Target)
-	if not isinstance(response, list):
-		print(response)
+    args = parser.parse_args()
+
+    try:
+        sources = [
+            SoftwareType[t]
+            for t in args.Target
+        ]
+        plex_ssn, sonarr_ssn = None, None
+
+        if SoftwareType.plex in sources:
+            if not (plex_base_url and plex_api_token):
+                raise ValueError("Plex not set up")
+
+            plex_ssn = Session()
+            plex_ssn.headers.update({'Accept': 'application/json'})
+            plex_ssn.params.update( # type: ignore
+                {'X-Plex-Token': plex_api_token})
+
+        elif SoftwareType.sonarr in sources:
+            if not (sonarr_base_url and sonarr_api_token):
+                raise ValueError("Sonarr not set up")
+
+            sonarr_ssn = Session()
+            sonarr_ssn.params.update( # type: ignore
+                {'apikey': sonarr_api_token}
+            )
+
+        sonarr_refresh_tba(
+            plex_ssn, sonarr_ssn,
+            sources
+        )
+
+    except ValueError as e:
+        parser.error(e.args[0])
